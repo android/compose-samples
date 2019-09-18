ab84ae837b9f6c6d0d5357833be023112cba3b20
Anna-Chiara Bellini <annachiarab@google.com>
2019-09-18 18:32:19 +0100

    Add support for previews
    Change-Id: If1b8dae2a3be5f0cc3a01ff91a77c4f2dcd74b73

--- JetNews/build.gradle.kts
+++ JetNews/build.gradle.kts
@@ -26,7 +26,7 @@ buildscript {
     }
 
     dependencies {
-        classpath("com.android.tools.build:gradle:3.5.0-beta05")
+        classpath("com.android.tools.build:gradle:3.5.0")
         classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:1.3.50-compose-20190806")
     }
 }
