345eb51818fdd281d64d0979ba6398ce258b693e
Anna-Chiara Bellini <annachiarab@google.com>
2019-10-21 12:27:46 -0700

    Update Gradle version and kotlin plugin version
    Change-Id: Ib8c97a6ca1df60a670c510b7c656d0491687cd72

--- JetNews/build.gradle.kts
+++ JetNews/build.gradle.kts
@@ -26,8 +26,8 @@ buildscript {
     }
 
     dependencies {
-        classpath("com.android.tools.build:gradle:3.5.0")
-        classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:1.3.60-withExperimentalGoogleExtensions-20191016")
+        classpath("com.android.tools.build:gradle:4.0.0-alpha01")
+        classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:1.3.60-eap-25")
     }
 }
 
