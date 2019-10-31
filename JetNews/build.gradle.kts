9406fc13060025a86706a2b851c9ed0b90413e22
Rafael Toledo <rafaeldtoledo@gmail.com>
2019-10-31 20:50:40 -0300

    Updates Kotlin to latest EAP (1.3.60-eap-76) and bumps KTlint

--- JetNews/build.gradle.kts
+++ JetNews/build.gradle.kts
@@ -24,7 +24,7 @@ buildscript {
     }
     dependencies {
         classpath("com.android.tools.build:gradle:4.0.0-alpha01")
-        classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:1.3.60-eap-25")
+        classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:1.3.60-eap-76")
     }
 }
 
