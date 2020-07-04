b47f081f622d738331997a37ad5882d47b5da32d
pandubaraja <pandu.baraja@gmail.com>
2020-07-04 19:45:52 +0700

    refactor: impl navigateTo and create loadFakePosts

--- JetNews/app/src/main/java/com/example/jetnews/ui/favorites/FavoritesScreen.kt
+++ JetNews/app/src/main/java/com/example/jetnews/ui/favorites/FavoritesScreen.kt
@@ -2,47 +2,57 @@ package com.example.jetnews.ui.favorites
 
 import androidx.compose.Composable
 import androidx.compose.remember
+import androidx.ui.core.ContextAmbient
 import androidx.ui.core.Modifier
-import androidx.ui.foundation.Clickable
 import androidx.ui.foundation.Icon
 import androidx.ui.foundation.Text
 import androidx.ui.foundation.VerticalScroller
+import androidx.ui.foundation.clickable
 import androidx.ui.layout.Column
 import androidx.ui.layout.Row
 import androidx.ui.layout.padding
 import androidx.ui.material.*
-import androidx.ui.material.ripple.ripple
 import androidx.ui.res.vectorResource
 import androidx.ui.tooling.preview.Preview
 import androidx.ui.unit.dp
 import com.example.jetnews.R
-import com.example.jetnews.data.posts
+import com.example.jetnews.data.posts.impl.BlockingFakePostsRepository
+import com.example.jetnews.data.successOr
 import com.example.jetnews.model.Post
-import com.example.jetnews.ui.*
+import com.example.jetnews.ui.AppDrawer
+import com.example.jetnews.ui.JetnewsStatus
+import com.example.jetnews.ui.Screen
+import com.example.jetnews.ui.ThemedPreview
 import com.example.jetnews.ui.home.*
 
 @Composable
-fun FavoritesScreen(scaffoldState: ScaffoldState = remember { ScaffoldState() }) {
+fun FavoritesScreen(
+        navigateTo: (Screen) -> Unit,
+        scaffoldState: ScaffoldState = remember { ScaffoldState() }
+) {
     Scaffold(
-        scaffoldState = scaffoldState,
-        drawerContent = {
-            AppDrawer(
-                currentScreen = Screen.Favorites,
-                closeDrawer = { scaffoldState.drawerState = DrawerState.Closed })
-        },
-        topAppBar = {
-            TopAppBar(
-                title = { Text("Favorites") },
-                navigationIcon = {
-                    IconButton(onClick = { scaffoldState.drawerState = DrawerState.Opened }) {
-                        Icon(vectorResource(R.drawable.ic_jetnews_logo))
-                    }
-                }
-            )
-        },
+            scaffoldState = scaffoldState,
+            drawerContent = {
+                AppDrawer(
+                        currentScreen = Screen.Favorites,
+                        closeDrawer = { scaffoldState.drawerState = DrawerState.Closed },
+                        navigateTo = navigateTo
+                )
+            },
+            topBar = {
+                TopAppBar(
+                        title = { Text("Favorites") },
+                        navigationIcon = {
+                            IconButton(onClick = { scaffoldState.drawerState = DrawerState.Opened }) {
+                                Icon(vectorResource(R.drawable.ic_jetnews_logo))
+                            }
+                        }
+                )
+            },
         bodyContent = {
             FavoritesScreenBody(
-                favorites = posts.filter { JetnewsStatus.favorites.contains(it.id) }
+                    favorites = loadFakePosts().filter { JetnewsStatus.favorites.contains(it.id) },
+                    navigateTo = navigateTo
             )
         }
     )
@@ -50,14 +60,18 @@ fun FavoritesScreen(scaffoldState: ScaffoldState = remember { ScaffoldState() })
 
 @Composable
 private fun FavoritesScreenBody(
-    favorites: List<Post>
+        favorites: List<Post>,
+        navigateTo: (Screen) -> Unit
 ){
     if(favorites.isNotEmpty()) {
         VerticalScroller {
             Column {
                 favorites.forEach {
                     Row {
-                        FavoriteCard(it)
+                        FavoriteCard(
+                                post = it,
+                                navigateTo = navigateTo
+                        )
                     }
                 }
             }
@@ -72,10 +86,12 @@ private fun FavoritesScreenBody(
 }
 
 @Composable
-fun FavoriteCard(post: Post) {
-    Clickable(
-        modifier = Modifier.ripple(),
-        onClick = { navigateTo(Screen.Article(postId = post.id), reset = false) }
+fun FavoriteCard(
+        post: Post,
+        navigateTo: (Screen) -> Unit
+) {
+    Column(
+            modifier = Modifier.clickable(onClick = { navigateTo(Screen.Article(post.id)) })
     ) {
         Row(modifier = Modifier.padding(16.dp)) {
             PostImage(post, Modifier.padding(end = 16.dp))
@@ -84,8 +100,8 @@ fun FavoriteCard(post: Post) {
                 AuthorAndReadTime(post)
             }
             FavoriteButton(
-                isFavorited = isFavorited(post.id),
-                onFavorite = { toggleFavorite(postId = post.id) }
+                    isFavorited = isFavorited(post.id),
+                    onFavorite = { toggleFavorite(postId = post.id) }
             )
         }
     }
@@ -95,15 +111,15 @@ fun FavoriteCard(post: Post) {
 @Composable
 fun PreviewFavoritesScreen() {
     ThemedPreview {
-        FavoritesScreen()
+        FavoritesScreen(navigateTo = {})
     }
 }
 
-
-@Preview("Favorites Screen Dark Theme")
 @Composable
-fun PreviewFavoritesScreenDark() {
-    ThemedPreview(darkThemeColors) {
-        FavoritesScreen()
+private fun loadFakePosts(): List<Post> {
+    var posts: List<Post> = emptyList()
+    BlockingFakePostsRepository(ContextAmbient.current).getPosts { result ->
+        posts = result.successOr(emptyList())
     }
+    return posts
 }
