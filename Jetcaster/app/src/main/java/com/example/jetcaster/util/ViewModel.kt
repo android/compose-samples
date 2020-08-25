dc9b832bc9830ed92af52627f44e75311cc3c175
Chris <chrisbanes@google.com>
2020-08-25 12:55:08 +0100

    [Jetcaster] Workaround Compiler issue with spaces in name
    Change-Id: I7eb87abd516716abcc040aaa6f5a2da5759e5540

--- Jetcaster/app/src/main/java/com/example/jetcaster/util/ViewModel.kt
+++ Jetcaster/app/src/main/java/com/example/jetcaster/util/ViewModel.kt
@@ -28,11 +28,18 @@ import androidx.lifecycle.ViewModelProvider
  */
 fun <VM : ViewModel> viewModelProviderFactoryOf(
     create: () -> VM
-): ViewModelProvider.Factory = object : ViewModelProvider.Factory {
-    @Suppress("UNCHECKED_CAST")
+): ViewModelProvider.Factory = SimpleFactory(create)
+
+/**
+ * This needs to be a named class currently to workaround a compiler issue: b/163807311
+ */
+private class SimpleFactory<VM : ViewModel>(
+    private val create: () -> VM
+) : ViewModelProvider.Factory {
     override fun <T : ViewModel> create(modelClass: Class<T>): T {
         val vm = create()
         if (modelClass.isInstance(vm)) {
+            @Suppress("UNCHECKED_CAST")
             return vm as T
         }
         throw IllegalArgumentException("Can not create ViewModel for class: $modelClass")
