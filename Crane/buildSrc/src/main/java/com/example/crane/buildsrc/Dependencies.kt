93547c654d07e8ee840bc0967e70a00747398223
Jose Alcerreca <jalc@google.com>
2020-08-26 20:13:21 +0200

    Updates accompanist to 0.2.0
    Change-Id: Ieca2829c6d56effd93ebc53daabe8dc759086f9b

--- Crane/buildSrc/src/main/java/com/example/crane/buildsrc/Dependencies.kt
+++ Crane/buildSrc/src/main/java/com/example/crane/buildsrc/Dependencies.kt
@@ -26,7 +26,7 @@ object Libs {
     const val googleMaps = "com.google.android.libraries.maps:maps:3.1.0-beta"
 
     object Accompanist {
-        private const val version = "0.1.10.ui-${AndroidX.Compose.snapshot}-SNAPSHOT"
+        private const val version = "0.2.0"
         const val coil = "dev.chrisbanes.accompanist:accompanist-coil:$version"
     }
 
