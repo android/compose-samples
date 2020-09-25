1eb5dca21f89ae3e6a3ff6fa3b38c1c22cd0576b
Florina Muntenescu <florinam@google.com>
2020-09-25 11:14:34 +0100

    [Jetsurvey] Alpha04 changes

--- Jetsurvey/app/src/main/java/com/example/compose/jetsurvey/MainActivity.kt
+++ Jetsurvey/app/src/main/java/com/example/compose/jetsurvey/MainActivity.kt
@@ -17,12 +17,12 @@
 package com.example.compose.jetsurvey
 
 import android.os.Bundle
-import androidx.activity.ComponentActivity
+import androidx.appcompat.app.AppCompatActivity
 import androidx.navigation.findNavController
 import androidx.navigation.ui.setupWithNavController
 import com.google.android.material.navigation.NavigationView
 
-class MainActivity : ComponentActivity() {
+class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
