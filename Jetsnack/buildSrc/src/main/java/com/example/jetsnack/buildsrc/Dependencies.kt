047fbe68b92c448b17a3f4677143da37451a37df
Jose Alcerreca <jalc@google.com>
2020-09-30 14:24:25 +0200

    Bump all AGPs to alpha13
    Change-Id: Ifc10d2808bc6bc0d2da848843451b7d82c599aa4

--- Jetsnack/buildSrc/src/main/java/com/example/jetsnack/buildsrc/Dependencies.kt
+++ Jetsnack/buildSrc/src/main/java/com/example/jetsnack/buildsrc/Dependencies.kt
@@ -21,7 +21,7 @@ object Versions {
 }
 
 object Libs {
-    const val androidGradlePlugin = "com.android.tools.build:gradle:4.2.0-alpha12"
+    const val androidGradlePlugin = "com.android.tools.build:gradle:4.2.0-alpha13"
     const val junit = "junit:junit:4.13"
 
     object Accompanist {
