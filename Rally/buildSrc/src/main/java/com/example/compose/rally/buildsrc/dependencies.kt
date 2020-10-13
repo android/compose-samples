4f1f1770b43619f604b398367ed883c518586a2b
Jose Alcerreca <jalc@google.com>
2020-10-13 12:51:11 +0200

    [Rally] Removes maven central snapshot repo
    Change-Id: I473f1f55b40e4c81fc655568828db3ab94fea0a6

--- Rally/buildSrc/src/main/java/com/example/compose/rally/buildsrc/dependencies.kt
+++ Rally/buildSrc/src/main/java/com/example/compose/rally/buildsrc/dependencies.kt
@@ -91,7 +91,6 @@ object Libs {
 }
 
 object Urls {
-    const val mavenCentralSnapshotRepo = "https://oss.sonatype.org/content/repositories/snapshots/"
     const val composeSnapshotRepo = "https://androidx-dev-prod.appspot.com/snapshots/builds/" +
         "${Libs.AndroidX.Compose.snapshot}/artifacts/ui/repository/"
 }
