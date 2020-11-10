40a4194c43e15268d7abf3683306ac952a44a514
Jolanda Verhoef <jolandaverhoef@google.com>
2020-11-10 10:40:57 +0100

    [Jetsnack] Update to snapshot 6961312

--- Jetsnack/buildSrc/src/main/java/com/example/jetsnack/buildsrc/Dependencies.kt
+++ Jetsnack/buildSrc/src/main/java/com/example/jetsnack/buildsrc/Dependencies.kt
@@ -47,7 +47,7 @@ object Libs {
         const val coreKtx = "androidx.core:core-ktx:1.5.0-alpha04"
 
         object Compose {
-            const val snapshot = "6953474"
+            const val snapshot = "6961312"
             const val version = "1.0.0-SNAPSHOT"
 
             const val foundation = "androidx.compose.foundation:foundation:${version}"
