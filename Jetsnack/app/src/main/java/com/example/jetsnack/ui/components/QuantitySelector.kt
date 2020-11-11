6fcd168c741ab050ebe6f9fb41bcba27b0bb1659
Jolanda Verhoef <jolandaverhoef@google.com>
2020-11-11 10:43:45 +0100

    [Jetsnack] Fix review comment

--- Jetsnack/app/src/main/java/com/example/jetsnack/ui/components/QuantitySelector.kt
+++ Jetsnack/app/src/main/java/com/example/jetsnack/ui/components/QuantitySelector.kt
@@ -20,12 +20,15 @@ import androidx.compose.animation.Crossfade
 import androidx.compose.foundation.layout.ChainStyle
 import androidx.compose.foundation.layout.ConstraintLayout
 import androidx.compose.foundation.layout.preferredWidthIn
+import androidx.compose.material.AmbientContentAlpha
+import androidx.compose.material.ContentAlpha
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Text
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.outlined.AddCircleOutline
 import androidx.compose.material.icons.outlined.RemoveCircleOutline
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.Providers
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.style.TextAlign
@@ -45,15 +48,17 @@ fun QuantitySelector(
     ConstraintLayout(modifier = modifier) {
         val (qty, minus, quantity, plus) = createRefs()
         createHorizontalChain(qty, minus, quantity, plus, chainStyle = ChainStyle.Packed)
-        Text(
-            text = stringResource(R.string.quantity),
-            style = MaterialTheme.typography.subtitle1,
-            color = JetsnackTheme.colors.textSecondary,
-            modifier = Modifier.constrainAs(qty) {
-                start.linkTo(parent.start)
-                linkTo(top = parent.top, bottom = parent.bottom)
-            }
-        )
+        Providers(AmbientContentAlpha provides ContentAlpha.medium) {
+            Text(
+                text = stringResource(R.string.quantity),
+                style = MaterialTheme.typography.subtitle1,
+                color = JetsnackTheme.colors.textSecondary,
+                modifier = Modifier.constrainAs(qty) {
+                    start.linkTo(parent.start)
+                    linkTo(top = parent.top, bottom = parent.bottom)
+                }
+            )
+        }
         JetsnackGradientTintedIconButton(
             asset = Icons.Outlined.RemoveCircleOutline,
             onClick = decreaseItemCount,
