48ad7bf909aca4e77f60c4071a395ba518ae5452
Manuel Vivo <manuelvicnt@gmail.com>
2021-01-08 13:31:55 +0100

    [Crane] Update to snapshot 7067732 (#342)

--- Crane/buildSrc/src/main/java/com/example/crane/buildsrc/Dependencies.kt
+++ Crane/buildSrc/src/main/java/com/example/crane/buildsrc/Dependencies.kt
@@ -21,7 +21,7 @@ object Versions {
 }
 
 object Libs {
-    const val androidGradlePlugin = "com.android.tools.build:gradle:7.0.0-alpha03"
+    const val androidGradlePlugin = "com.android.tools.build:gradle:7.0.0-alpha04"
     const val ktLint = "com.pinterest:ktlint:${Versions.ktLint}"
 
     object GoogleMaps {
@@ -30,7 +30,7 @@ object Libs {
     }
 
     object Accompanist {
-        private const val version = "0.4.1"
+        private const val version = "0.4.1.compose-7067732-SNAPSHOT"
         const val coil = "dev.chrisbanes.accompanist:accompanist-coil:$version"
     }
 
@@ -49,8 +49,8 @@ object Libs {
 
     object AndroidX {
         object Compose {
-            const val snapshot = ""
-            private const val version = "1.0.0-alpha09"
+            const val snapshot = "7067732"
+            private const val version = "1.0.0-SNAPSHOT"
 
             const val runtime = "androidx.compose.runtime:runtime:$version"
             const val runtimeLivedata = "androidx.compose.runtime:runtime-livedata:$version"
