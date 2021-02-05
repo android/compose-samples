606999e193e8e3d24a6e8cfaf2d5aecf64be2147
Jose Alcérreca <jalc@google.com>
2021-02-05 10:59:04 +0000

    [Rally] Updates Rally to alpha12 snapshot

--- Rally/app/src/main/java/com/example/compose/rally/RallyActivity.kt
+++ Rally/app/src/main/java/com/example/compose/rally/RallyActivity.kt
@@ -18,15 +18,16 @@ package com.example.compose.rally
 
 import android.os.Bundle
 import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material.Scaffold
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
-import androidx.compose.runtime.savedinstancestate.savedInstanceState
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.platform.setContent
 import com.example.compose.rally.ui.components.RallyTopAppBar
 import com.example.compose.rally.ui.theme.RallyTheme
 
@@ -47,7 +48,7 @@ class RallyActivity : ComponentActivity() {
 fun RallyApp() {
     RallyTheme {
         val allScreens = RallyScreen.values().toList()
-        var currentScreen by savedInstanceState { RallyScreen.Overview }
+        var currentScreen by rememberSaveable { mutableStateOf(RallyScreen.Overview) }
         Scaffold(
             topBar = {
                 RallyTopAppBar(
