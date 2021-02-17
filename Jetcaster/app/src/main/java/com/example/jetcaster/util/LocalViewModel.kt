ef07b941bf25ebf29e9783d66c014754355ea1ab
Yacine Rezgui <yrezgui@google.com>
2021-02-17 19:54:41 +0000

    Change FIXME to TODO (spotless wasn't happy)

--- Jetcaster/app/src/main/java/com/example/jetcaster/util/LocalViewModel.kt
+++ Jetcaster/app/src/main/java/com/example/jetcaster/util/LocalViewModel.kt
@@ -1,13 +1,28 @@
+/*
+ * Copyright 2021 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package com.example.jetcaster.util
 
-// FIXME: This is a temporary fix, delete this file and import the dependency from androidx package instead
-
-import androidx.lifecycle.viewmodel.compose.LocalViewModelStoreOwner
+// TODO: This is a temporary fix, delete this file and import the dependency from androidx package instead
 
 import androidx.compose.runtime.Composable
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.ViewModelProvider
 import androidx.lifecycle.ViewModelStoreOwner
+import androidx.lifecycle.viewmodel.compose.LocalViewModelStoreOwner
 
 /**
  * Returns an existing [ViewModel] or creates a new one in the scope (usually, a fragment or
