d3af51271f81e86cc76de00eeeef3a9d612ff038
Florina Muntenescu <florinam@google.com>
2021-02-23 15:26:04 +0000

    [All] Update to beta01

--- Owl/buildSrc/src/main/java/com/example/owl/buildsrc/Dependencies.kt
+++ Owl/buildSrc/src/main/java/com/example/owl/buildsrc/Dependencies.kt
@@ -21,10 +21,10 @@ object Versions {
 }
 
 object Libs {
-    const val androidGradlePlugin = "com.android.tools.build:gradle:7.0.0-alpha07"
+    const val androidGradlePlugin = "com.android.tools.build:gradle:7.0.0-alpha08"
 
     object Accompanist {
-        private const val version = "0.5.2.compose-7154166-SNAPSHOT"
+        private const val version = "0.6.0"
         const val coil = "dev.chrisbanes.accompanist:accompanist-coil:$version"
         const val insets = "dev.chrisbanes.accompanist:accompanist-insets:$version"
     }
@@ -50,11 +50,11 @@ object Libs {
 
     object AndroidX {
         const val coreKtx = "androidx.core:core-ktx:1.5.0-beta01"
-        const val navigation = "androidx.navigation:navigation-compose:1.0.0-SNAPSHOT"
+        const val navigation = "androidx.navigation:navigation-compose:1.0.0-alpha08"
 
         object Compose {
-            const val snapshot = "7154166"
-            const val version = "1.0.0-SNAPSHOT"
+            const val snapshot = ""
+            const val version = "1.0.0-beta01"
 
             const val animation = "androidx.compose.animation:animation:$version"
             const val foundation = "androidx.compose.foundation:foundation:$version"
@@ -69,12 +69,12 @@ object Libs {
         }
 
         object Activity {
-            const val activityCompose = "androidx.activity:activity-compose:1.3.0-SNAPSHOT"
+            const val activityCompose = "androidx.activity:activity-compose:1.3.0-alpha03"
         }
 
         object ConstraintLayout {
             const val constraintLayoutCompose =
-                "androidx.constraintlayout:constraintlayout-compose:1.0.0-SNAPSHOT"
+                "androidx.constraintlayout:constraintlayout-compose:1.0.0-alpha03"
         }
 
         object Test {
