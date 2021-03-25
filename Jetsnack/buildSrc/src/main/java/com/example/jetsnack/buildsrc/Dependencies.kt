5e0b1e7e9b9abde60e11f36b811bd2875344cb11
Chris Banes <chrisbanes@google.com>
2021-03-25 14:55:13 +0000

    [Jetsnack] Add preview placeholder to images
    Use CoilImage's new previewPlaceholder to display
    a placeholder when in preview mode
    
    Change-Id: I8e22fbe225fea2921e4d54e5fb09bac7a61cb09e

--- Jetsnack/buildSrc/src/main/java/com/example/jetsnack/buildsrc/Dependencies.kt
+++ Jetsnack/buildSrc/src/main/java/com/example/jetsnack/buildsrc/Dependencies.kt
@@ -24,7 +24,7 @@ object Libs {
     const val androidGradlePlugin = "com.android.tools.build:gradle:7.0.0-alpha11"
 
     object Accompanist {
-        private const val version = "0.7.0"
+        const val version = "0.7.1-SNAPSHOT"
         const val coil = "com.google.accompanist:accompanist-coil:$version"
         const val insets = "com.google.accompanist:accompanist-insets:$version"
     }
