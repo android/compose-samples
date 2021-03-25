7a1bcf85e22adebfcbfeb2934aabbd9f089e011c
Chris Banes <chrisbanes@users.noreply.github.com>
2021-03-25 09:43:43 +0000

    [All] Update to AGP 7.0.0-alpha11 (#452)

--- Crane/buildSrc/src/main/java/com/example/crane/buildsrc/Dependencies.kt
+++ Crane/buildSrc/src/main/java/com/example/crane/buildsrc/Dependencies.kt
@@ -21,7 +21,7 @@ object Versions {
 }
 
 object Libs {
-    const val androidGradlePlugin = "com.android.tools.build:gradle:7.0.0-alpha10"
+    const val androidGradlePlugin = "com.android.tools.build:gradle:7.0.0-alpha11"
     const val ktLint = "com.pinterest:ktlint:${Versions.ktLint}"
 
     object GoogleMaps {
--- Jetsnack/buildSrc/src/main/java/com/example/jetsnack/buildsrc/Dependencies.kt
+++ Jetsnack/buildSrc/src/main/java/com/example/jetsnack/buildsrc/Dependencies.kt
@@ -21,7 +21,7 @@ object Versions {
 }
 
 object Libs {
-    const val androidGradlePlugin = "com.android.tools.build:gradle:7.0.0-alpha10"
+    const val androidGradlePlugin = "com.android.tools.build:gradle:7.0.0-alpha11"
 
     object Accompanist {
         private const val version = "0.7.0"
