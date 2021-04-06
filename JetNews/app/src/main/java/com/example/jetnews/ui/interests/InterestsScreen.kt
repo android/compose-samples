5575a951f68bc8a8ec2e08da42c836552b1906fb
Angeles <angeles.bilbao6@gmail.com>
2021-04-06 11:52:51 -0500

    Removed wildcard imports from interests screen

--- JetNews/app/src/main/java/com/example/jetnews/ui/interests/InterestsScreen.kt
+++ JetNews/app/src/main/java/com/example/jetnews/ui/interests/InterestsScreen.kt
@@ -17,7 +17,13 @@
 package com.example.jetnews.ui.interests
 
 import androidx.compose.foundation.Image
-import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
 import androidx.compose.foundation.selection.toggleable
