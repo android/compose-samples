af6c73545ee3e95b76c953ffd0d17ae92ff063df
Angeles <angeles.bilbao6@gmail.com>
2021-04-06 10:36:14 -0500

    Removed unnecessary imports

--- JetNews/app/src/main/java/com/example/jetnews/ui/article/ArticleScreen.kt
+++ JetNews/app/src/main/java/com/example/jetnews/ui/article/ArticleScreen.kt
@@ -18,11 +18,6 @@ package com.example.jetnews.ui.article
 
 import android.content.Context
 import android.content.Intent
-import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.Spacer
-import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.height
-import androidx.compose.foundation.layout.padding
 import androidx.compose.material.AlertDialog
 import androidx.compose.material.Icon
 import androidx.compose.material.IconButton
