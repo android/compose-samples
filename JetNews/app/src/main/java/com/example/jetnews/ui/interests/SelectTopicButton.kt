dfa7e30dc06f016123564a874d7eb9145ccb5a16
Angeles <angeles.bilbao6@gmail.com>
2021-04-08 13:30:08 -0500

    [Jetnews] UI polish bugs #458
    Updated UI components to match current design

--- JetNews/app/src/main/java/com/example/jetnews/ui/interests/SelectTopicButton.kt
+++ JetNews/app/src/main/java/com/example/jetnews/ui/interests/SelectTopicButton.kt
@@ -16,10 +16,11 @@
 
 package com.example.jetnews.ui.interests
 
+import androidx.compose.foundation.BorderStroke
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.shape.CircleShape
-import androidx.compose.material.Icon
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.material.icons.Icons
@@ -27,6 +28,7 @@ import androidx.compose.material.icons.filled.Add
 import androidx.compose.material.icons.filled.Done
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.ColorFilter
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.example.jetnews.ui.ThemedPreview
@@ -37,18 +39,23 @@ fun SelectTopicButton(
     selected: Boolean = false
 ) {
     val icon = if (selected) Icons.Filled.Done else Icons.Filled.Add
+    val iconColor = if (selected) MaterialTheme.colors.onPrimary else MaterialTheme.colors.primary
+    val borderColor = if (selected) MaterialTheme.colors.primary else MaterialTheme.colors.onSurface.copy(alpha = 0.1f)
     val backgroundColor = if (selected) {
         MaterialTheme.colors.primary
     } else {
-        MaterialTheme.colors.onSurface.copy(alpha = 0.12f)
+        MaterialTheme.colors.onPrimary
     }
     Surface(
         color = backgroundColor,
         shape = CircleShape,
+        border = BorderStroke(1.dp, borderColor),
         modifier = modifier.size(36.dp, 36.dp)
     ) {
-        Icon(
+        Image(
             imageVector = icon,
+            colorFilter = ColorFilter.tint(iconColor),
+            modifier = Modifier.padding(8.dp),
             contentDescription = null // toggleable at higher level
         )
     }
