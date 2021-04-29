8182fac1666fb83e806b4994fd1874b500f1af52
Manuel Vivo <mvivo@google.com>
2021-04-29 13:30:16 +0000

    [Crane] Fix ignored test with new APIs

--- Crane/app/src/main/java/androidx/compose/samples/crane/calendar/Calendar.kt
+++ Crane/app/src/main/java/androidx/compose/samples/crane/calendar/Calendar.kt
@@ -224,8 +224,12 @@ private fun LazyListScope.itemsCalendarMonth(
     item {
         DaysOfWeek(modifier = contentModifier)
     }
-    for (week in month.weeks.value) {
-        item {
+
+    month.weeks.value.forEachIndexed { index, week ->
+        // A custom key needs to be given to these items so that they can be found in tests that
+        // need scrolling. The format of the key is ${year/month/weekNumber}. Thus,
+        // the key for the fourth week of December 2020 is "2020/12/4"
+        item(key = "${month.year}/${month.monthNumber}/${index + 1}") {
             Week(
                 modifier = contentModifier,
                 week = week,
