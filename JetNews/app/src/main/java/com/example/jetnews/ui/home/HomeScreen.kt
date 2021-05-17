2d3a6802a777932eedcb087d46f57dc4516d5cb4
Angeles <angeles.bilbao6@gmail.com>
2021-05-17 11:44:44 -0500

    Spotless apply

--- JetNews/app/src/main/java/com/example/jetnews/ui/home/HomeScreen.kt
+++ JetNews/app/src/main/java/com/example/jetnews/ui/home/HomeScreen.kt
@@ -16,8 +16,8 @@
 
 package com.example.jetnews.ui.home
 
-import androidx.compose.foundation.Image
 import android.content.res.Configuration.UI_MODE_NIGHT_YES
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
