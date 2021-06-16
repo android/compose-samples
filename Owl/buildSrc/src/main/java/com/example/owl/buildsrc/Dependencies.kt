4ab5938123e2cc9563fa284e2b1ad00ff044fc21
Yacine Rezgui <rezgui.y@gmail.com>
2021-06-16 23:30:39 +0100

    Update Owl to beta09

--- Owl/buildSrc/src/main/java/com/example/owl/buildsrc/Dependencies.kt
+++ Owl/buildSrc/src/main/java/com/example/owl/buildsrc/Dependencies.kt
@@ -24,7 +24,7 @@ object Libs {
     const val androidGradlePlugin = "com.android.tools.build:gradle:7.0.0-beta03"
 
     object Accompanist {
-        const val version = "0.11.0"
+        const val version = "0.12.0"
         const val coil = "com.google.accompanist:accompanist-coil:$version"
         const val insets = "com.google.accompanist:accompanist-insets:$version"
     }
@@ -53,12 +53,12 @@ object Libs {
         const val navigation = "androidx.navigation:navigation-compose:2.4.0-alpha02"
 
         object Activity {
-            const val activityCompose = "androidx.activity:activity-compose:1.3.0-beta01"
+            const val activityCompose = "androidx.activity:activity-compose:1.3.0-beta02"
         }
 
         object Compose {
             const val snapshot = ""
-            const val version = "1.0.0-beta08"
+            const val version = "1.0.0-beta09"
 
             const val animation = "androidx.compose.animation:animation:$version"
             const val foundation = "androidx.compose.foundation:foundation:$version"
@@ -74,7 +74,7 @@ object Libs {
 
         object ConstraintLayout {
             const val constraintLayoutCompose =
-                "androidx.constraintlayout:constraintlayout-compose:1.0.0-alpha07"
+                "androidx.constraintlayout:constraintlayout-compose:1.0.0-alpha08"
         }
 
         object Test {
