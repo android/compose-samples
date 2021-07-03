08b48d60607f8e949d2c764e56202cb77cdde24b
Abrar Wiryawan <abrarwiryawan@gmail.com>
2021-07-03 09:46:02 +0700

    Update Dependencies.kt
    bump junit to 1.1.3

--- Jetsnack/buildSrc/src/main/java/com/example/jetsnack/buildsrc/Dependencies.kt
+++ Jetsnack/buildSrc/src/main/java/com/example/jetsnack/buildsrc/Dependencies.kt
@@ -86,7 +86,7 @@ object Libs {
             const val runner = "androidx.test:runner:$version"
             const val rules = "androidx.test:rules:$version"
             object Ext {
-                private const val version = "1.1.2-rc01"
+                private const val version = "1.1.3"
                 const val junit = "androidx.test.ext:junit-ktx:$version"
             }
             const val espressoCore = "androidx.test.espresso:espresso-core:3.4.0"
