435b83ec53f5397d8266cee2af44d6d9338cbf72
Colin White <colin@colinwhite.me>
2021-07-21 13:27:35 -0500

    PR feedback.

--- Crane/app/src/main/java/androidx/compose/samples/crane/CraneApplication.kt
+++ Crane/app/src/main/java/androidx/compose/samples/crane/CraneApplication.kt
@@ -20,11 +20,16 @@ import android.app.Application
 import androidx.compose.samples.crane.util.UnsplashSizingInterceptor
 import coil.ImageLoader
 import coil.ImageLoaderFactory
+import coil.compose.rememberImagePainter
 import dagger.hilt.android.HiltAndroidApp
 
 @HiltAndroidApp
 class CraneApplication : Application(), ImageLoaderFactory {
 
+    /**
+     * Create the singleton [ImageLoader].
+     * This is used by [rememberImagePainter] to load images in the app.
+     */
     override fun newImageLoader(): ImageLoader {
         return ImageLoader.Builder(this)
             .componentRegistry {
--- Owl/app/src/main/java/com/example/owl/OwlApplication.kt
+++ Owl/app/src/main/java/com/example/owl/OwlApplication.kt
@@ -3,11 +3,16 @@ package com.example.owl
 import android.app.Application
 import coil.ImageLoader
 import coil.ImageLoaderFactory
+import coil.compose.rememberImagePainter
 import com.example.owl.ui.utils.UnsplashSizingInterceptor
 
 @Suppress("unused")
 class OwlApplication : Application(), ImageLoaderFactory {
 
+    /**
+     * Create the singleton [ImageLoader].
+     * This is used by [rememberImagePainter] to load images in the app.
+     */
     override fun newImageLoader(): ImageLoader {
         return ImageLoader.Builder(this)
             .componentRegistry {
