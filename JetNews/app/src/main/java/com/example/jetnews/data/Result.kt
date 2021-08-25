920c95dd484cc94b6afaf219adfbaa3ec4dd3df6
Manuel Vivo <mvivo@google.com>
2021-08-25 15:54:36 +0000

    [Jetnews] Cleanup unused classes

--- JetNews/app/src/main/java/com/example/jetnews/data/Result.kt
+++ JetNews/app/src/main/java/com/example/jetnews/data/Result.kt
@@ -20,7 +20,6 @@ package com.example.jetnews.data
  * A generic class that holds a value or an exception
  */
 sealed class Result<out R> {
-
     data class Success<out T>(val data: T) : Result<T>()
     data class Error(val exception: Exception) : Result<Nothing>()
 }
