0cabc2f5f50c86de8d4bf622e75ce9091233970c
Alex Vanyo <vanyo@google.com>
2021-09-29 11:31:27 -0700

    Re-order WindowSize functions

--- JetNews/app/src/main/java/com/example/jetnews/utils/WindowSize.kt
+++ JetNews/app/src/main/java/com/example/jetnews/utils/WindowSize.kt
@@ -39,31 +39,6 @@ import androidx.window.layout.WindowMetricsCalculator
 enum class WindowSize { Compact, Medium, Expanded }
 
 /**
- * Partitions a [DpSize] into a enumerated [WindowSize] class.
- */
-@VisibleForTesting
-fun getWindowSizeClass(windowDpSize: DpSize): WindowSize = when {
-    windowDpSize.width < 0.dp -> throw IllegalArgumentException("Dp value cannot be negative")
-    windowDpSize.width < 600.dp -> WindowSize.Compact
-    windowDpSize.width < 840.dp -> WindowSize.Medium
-    else -> WindowSize.Expanded
-}
-
-/**
- * Remembers the [Size] in pixels of the window corresponding to the current window metrics.
- */
-@Composable
-private fun Activity.rememberWindowSize(): Size {
-    val configuration = LocalConfiguration.current
-    // WindowMetricsCalculator implicitly depends on the configuration through the activity,
-    // so recalculate it upon changes.
-    val windowMetrics = remember(configuration) {
-        WindowMetricsCalculator.getOrCreate().computeCurrentWindowMetrics(this)
-    }
-    return windowMetrics.bounds.toComposeRect().size
-}
-
-/**
  * Remembers the [WindowSize] class for the window corresponding to the current window metrics.
  */
 @Composable
@@ -78,4 +53,30 @@ fun Activity.rememberWindowSizeClass(): WindowSize {
 
     // Calculate the window size class
     return getWindowSizeClass(windowDpSize)
+}
+
+/**
+ * Remembers the [Size] in pixels of the window corresponding to the current window metrics.
+ */
+@Composable
+private fun Activity.rememberWindowSize(): Size {
+    val configuration = LocalConfiguration.current
+    // WindowMetricsCalculator implicitly depends on the configuration through the activity,
+    // so re-calculate it upon changes.
+    val windowMetrics = remember(configuration) {
+        WindowMetricsCalculator.getOrCreate().computeCurrentWindowMetrics(this)
+    }
+    return windowMetrics.bounds.toComposeRect().size
+}
+
+
+/**
+ * Partitions a [DpSize] into a enumerated [WindowSize] class.
+ */
+@VisibleForTesting
+fun getWindowSizeClass(windowDpSize: DpSize): WindowSize = when {
+    windowDpSize.width < 0.dp -> throw IllegalArgumentException("Dp value cannot be negative")
+    windowDpSize.width < 600.dp -> WindowSize.Compact
+    windowDpSize.width < 840.dp -> WindowSize.Medium
+    else -> WindowSize.Expanded
 }
