fa019e3a5311f46e189460c17ef603b56f728faa
Manuel Vivo <mvivo@google.com>
2021-10-05 13:06:02 +0000

    PR comments

--- Jetcaster/app/src/main/java/com/example/jetcaster/ui/JetcasterAppState.kt
+++ Jetcaster/app/src/main/java/com/example/jetcaster/ui/JetcasterAppState.kt
@@ -26,6 +26,7 @@ import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.platform.LocalContext
+import androidx.core.content.ContextCompat.getSystemService
 import androidx.navigation.NavHostController
 import androidx.navigation.compose.rememberNavController
 
@@ -60,14 +61,14 @@ class JetcasterAppState(
 
     @Suppress("DEPRECATION")
     private fun checkIfOnline(): Boolean {
-        val cm = context.getSystemService(Context.CONNECTIVITY_SERVICE) as ConnectivityManager
+        val cm = getSystemService(context, ConnectivityManager::class.java)
 
         return if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
-            val capabilities = cm.getNetworkCapabilities(cm.activeNetwork) ?: return false
+            val capabilities = cm?.getNetworkCapabilities(cm.activeNetwork) ?: return false
             capabilities.hasCapability(NetworkCapabilities.NET_CAPABILITY_INTERNET) &&
                 capabilities.hasCapability(NetworkCapabilities.NET_CAPABILITY_VALIDATED)
         } else {
-            cm.activeNetworkInfo?.isConnectedOrConnecting == true
+            cm?.activeNetworkInfo?.isConnectedOrConnecting == true
         }
     }
 }
