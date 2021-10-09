b648324de30c6541e9444f340d8052399fb65c4a
Patrick Fuentes <fuentesp@google.com>
2021-10-09 10:12:22 -0700

    Make spotless happy

--- JetNews/app/src/main/java/com/example/jetnews/ui/modifiers/KeyEvents.kt
+++ JetNews/app/src/main/java/com/example/jetnews/ui/modifiers/KeyEvents.kt
@@ -1,4 +1,20 @@
-package com.example.jetnews.ui.utils
+/*
+ * Copyright 2021 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.example.jetnews.ui.modifiers
 
 import androidx.compose.ui.ExperimentalComposeUiApi
 import androidx.compose.ui.Modifier
@@ -9,14 +25,13 @@ import androidx.compose.ui.input.key.onPreviewKeyEvent
 /**
  * Note: this method intercepts the key event rather than passing it on to children
  */
-fun Modifier.interceptKey(key: Key, onKeyEvent: ()-> Unit): Modifier {
+fun Modifier.interceptKey(key: Key, onKeyEvent: () -> Unit): Modifier {
     return this.onPreviewKeyEvent {
         @OptIn(ExperimentalComposeUiApi::class)
-        if(it.key == key) {
+        if (it.key == key) {
             onKeyEvent()
             true
-        }
-        else {
+        } else {
             false
         }
     }
