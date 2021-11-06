0bfa6a43e25bf534b545d28d2a317ebf2e730447
Abhimanyu14 <abhimanyu.n14@gmail.com>
2021-11-07 00:23:39 +0530

    upgraded room to 2.4.0-beta01

--- Jetcaster/buildSrc/src/main/java/com/example/jetcaster/buildsrc/dependencies.kt
+++ Jetcaster/buildSrc/src/main/java/com/example/jetcaster/buildsrc/dependencies.kt
@@ -113,7 +113,7 @@ object Libs {
         }
 
         object Room {
-            private const val version = "2.3.0"
+            private const val version = "2.4.0-beta01"
             const val runtime = "androidx.room:room-runtime:${version}"
             const val ktx = "androidx.room:room-ktx:${version}"
             const val compiler = "androidx.room:room-compiler:${version}"
