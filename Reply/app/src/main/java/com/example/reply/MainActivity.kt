19047f946e12448e93b16183af162c357382a072
Gurupreet Singh <gurusingh@google.com>
2022-03-10 13:24:36 +0100

    added adaptive size class

--- Reply/app/src/main/java/com/example/reply/MainActivity.kt
+++ Reply/app/src/main/java/com/example/reply/MainActivity.kt
@@ -10,6 +10,7 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import com.example.reply.ui.rememberWindowSizeClass
 import com.example.reply.ui.theme.ReplyTheme
 
 class MainActivity : ComponentActivity() {
@@ -17,27 +18,17 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         setContent {
             ReplyTheme {
-                // A surface container using the 'background' color from the theme
-                Surface(
-                    modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colorScheme.background
-                ) {
-                    Greeting("Android")
-                }
+                val windowSize = rememberWindowSizeClass()
+                ReplyApp(windowSize)
             }
         }
     }
-}
-
-@Composable
-fun Greeting(name: String) {
-    Text(text = "Hello $name!")
 }
 
 @Preview(showBackground = true)
 @Composable
 fun DefaultPreview() {
     ReplyTheme {
-        Greeting("Android")
+
     }
 }
