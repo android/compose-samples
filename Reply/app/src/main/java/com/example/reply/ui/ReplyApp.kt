06ca52590b7cd0f0563712242d64d829e00db9c6
Gurupreet Singh <gurusingh@google.com>
2022-03-15 13:10:07 +0100

    added comments and cleanup

--- Reply/app/src/main/java/com/example/reply/ui/ReplyApp.kt
+++ Reply/app/src/main/java/com/example/reply/ui/ReplyApp.kt
@@ -3,7 +3,9 @@ package com.example.reply.ui
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material3.*
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.PermanentNavigationDrawer
+import androidx.compose.material3.ModalNavigationDrawer
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
 import androidx.compose.ui.Modifier
@@ -17,6 +19,10 @@ fun ReplyApp(
 ) {
     val foldingDevicePosture = devicePosture.collectAsState().value
 
+    /**
+     * This will help us select type of navigation and content type depending on window size and
+     * fold state of the device.
+     */
     val (navigationType, contentType) = when (windowSize) {
         WindowSize.Compact -> {
             Pair(ReplyNavigationType.BOTTOM_NAVIGATION, ReplyContentType.LIST_ONLY)
