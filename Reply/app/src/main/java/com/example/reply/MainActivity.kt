e1b287d7cca21edd68464123e905d111fcbd5e33
Gurupreet Singh <gurusingh@google.com>
2022-03-15 12:52:00 +0100

    added reply size and fold setup

--- Reply/app/src/main/java/com/example/reply/MainActivity.kt
+++ Reply/app/src/main/java/com/example/reply/MainActivity.kt
@@ -3,23 +3,50 @@ package com.example.reply
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Surface
-import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
-import com.example.reply.ui.rememberWindowSizeClass
+import androidx.lifecycle.flowWithLifecycle
+import androidx.lifecycle.lifecycleScope
+import androidx.window.layout.FoldingFeature
+import androidx.window.layout.WindowInfoTracker.Companion.getOrCreate
+import com.example.reply.ui.*
 import com.example.reply.ui.theme.ReplyTheme
+import kotlinx.coroutines.flow.SharingStarted
+import kotlinx.coroutines.flow.map
+import kotlinx.coroutines.flow.stateIn
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
+        /**
+         * Flow of [DevicePosture] that emits every time there's a change in the windowLayoutInfo
+         */
+        val devicePosture = getOrCreate(this).windowLayoutInfo(this)
+            .flowWithLifecycle(this.lifecycle)
+            .map { layoutInfo ->
+                val foldingFeature =
+                    layoutInfo.displayFeatures.filterIsInstance<FoldingFeature>().firstOrNull()
+                when {
+                    isTableTopPosture(foldingFeature) ->
+                        DevicePosture.TableTopPosture(foldingFeature.bounds)
+                    isBookPosture(foldingFeature) ->
+                        DevicePosture.BookPosture(foldingFeature.bounds)
+                    isSeparating(foldingFeature) ->
+                        DevicePosture.Separating(foldingFeature.bounds, foldingFeature.orientation)
+                    else -> DevicePosture.NormalPosture
+                }
+            }
+            .stateIn(
+                scope = lifecycleScope,
+                started = SharingStarted.Eagerly,
+                initialValue = DevicePosture.NormalPosture
+            )
+
         setContent {
             ReplyTheme {
                 val windowSize = rememberWindowSizeClass()
-                ReplyApp(windowSize)
+                ReplyApp(windowSize, devicePosture)
             }
         }
     }
--- Reply/app/src/main/java/com/example/reply/ui/ReplyApp.kt
+++ Reply/app/src/main/java/com/example/reply/ui/ReplyApp.kt
@@ -1,11 +1,65 @@
-package com.example.reply
+package com.example.reply.ui
 
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.*
 import androidx.compose.runtime.Composable
-import com.example.reply.ui.WindowSize
+import androidx.compose.runtime.collectAsState
+import androidx.compose.ui.Modifier
+import kotlinx.coroutines.flow.StateFlow
 
+@OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun ReplyApp(
-    windowSize: WindowSize
+    windowSize: WindowSize,
+    devicePosture: StateFlow<DevicePosture>
 ) {
+    val foldingDevicePosture = devicePosture.collectAsState().value
 
+    val (navigationType, contentType) = when (windowSize) {
+        WindowSize.Compact -> {
+            Pair(ReplyNavigationType.BOTTOM_NAVIGATION, ReplyContentType.LIST_ONLY)
+        }
+        WindowSize.Medium -> {
+            if (foldingDevicePosture != DevicePosture.NormalPosture) {
+                Pair(ReplyNavigationType.NAVIGATION_RAIL, ReplyContentType.LIST_AND_DETAIL)
+            } else {
+                Pair(ReplyNavigationType.NAVIGATION_RAIL, ReplyContentType.LIST_ONLY)
+            }
+        }
+        WindowSize.Expanded -> {
+            Pair(ReplyNavigationType.FIXED_NAVIGATION_DRAWER, ReplyContentType.LIST_AND_DETAIL)
+        }
+    }
+
+    if (navigationType == ReplyNavigationType.FIXED_NAVIGATION_DRAWER) {
+        PermanentNavigationDrawer(drawerContent = {}) {
+            ReplyAppContent(navigationType, contentType)
+        }
+    } else {
+        ModalNavigationDrawer(drawerContent = {}) {
+            ReplyAppContent(navigationType, contentType)
+        }
+    }
+}
+
+@Composable
+fun ReplyAppContent(navigationType: ReplyNavigationType, contentType: ReplyContentType) {
+    Row(modifier = Modifier
+        .fillMaxSize()) {
+        if (navigationType == ReplyNavigationType.NAVIGATION_RAIL) {
+            //ReplyNavigationRail()
+        }
+        Column(modifier = Modifier.fillMaxSize()) {
+            if (contentType == ReplyContentType.LIST_AND_DETAIL) {
+                // ReplyListAndDetailContent()
+            } else {
+                // ReplyListContent()
+            }
+            if (navigationType == ReplyNavigationType.BOTTOM_NAVIGATION) {
+                //ReplyBottomNavigation()
+            }
+        }
+    }
 }
