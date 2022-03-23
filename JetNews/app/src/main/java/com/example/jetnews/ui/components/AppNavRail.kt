14ed1f358680f2eec18a16743d29699a11eaa3c4
Ian G. Clifton <IanGClifton@users.noreply.github.com>
2022-03-23 14:46:39 -0700

    Removed unused import

--- JetNews/app/src/main/java/com/example/jetnews/ui/components/AppNavRail.kt
+++ JetNews/app/src/main/java/com/example/jetnews/ui/components/AppNavRail.kt
@@ -40,7 +40,6 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.vector.ImageVector
 import androidx.compose.ui.res.stringResource
-import androidx.compose.ui.semantics.Role
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.example.jetnews.R
