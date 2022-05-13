0476bc0986edfe6fc76cd80fbb8aad8034742860
Vladislav Daniliuk <shapeofvoice2001@gmail.com>
2022-05-13 12:34:18 +0300

    - Update click effect for ChoiceChipContent.

--- Jetcaster/app/src/main/java/com/example/jetcaster/ui/home/discover/Discover.kt
+++ Jetcaster/app/src/main/java/com/example/jetcaster/ui/home/discover/Discover.kt
@@ -17,6 +17,7 @@
 package com.example.jetcaster.ui.home.discover
 
 import androidx.compose.animation.Crossfade
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
@@ -26,13 +27,14 @@ import androidx.compose.foundation.layout.padding
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.ScrollableTabRow
 import androidx.compose.material.Surface
-import androidx.compose.material.Tab
 import androidx.compose.material.TabPosition
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
 import androidx.lifecycle.viewmodel.compose.viewModel
 import com.example.jetcaster.data.Category
@@ -100,16 +102,16 @@ private fun PodcastCategoryTabs(
         modifier = modifier
     ) {
         categories.forEachIndexed { index, category ->
-            Tab(
+            ChoiceChipContent(
+                text = category.name,
                 selected = index == selectedIndex,
-                onClick = { onCategorySelected(category) }
-            ) {
-                ChoiceChipContent(
-                    text = category.name,
-                    selected = index == selectedIndex,
-                    modifier = Modifier.padding(horizontal = 4.dp, vertical = 16.dp)
-                )
-            }
+                modifier = Modifier
+                    .padding(horizontal = 4.dp, vertical = 16.dp)
+                    .clip(MaterialTheme.shapes.small)
+                    .clickable {
+                        onCategorySelected(category)
+                    }
+            )
         }
     }
 }
@@ -134,6 +136,7 @@ private fun ChoiceChipContent(
     ) {
         Text(
             text = text,
+            textAlign = TextAlign.Center,
             style = MaterialTheme.typography.body2,
             modifier = Modifier.padding(horizontal = 16.dp, vertical = 8.dp)
         )
