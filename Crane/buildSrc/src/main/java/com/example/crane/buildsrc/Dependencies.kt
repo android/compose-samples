f02e32d5b2f0dd9d9c0d8d9e0327448a6d079000
Alex Vanyo <vanyo@google.com>
2022-05-13 15:32:51 -0700

    Fix Crane Dagger lint

--- Crane/buildSrc/src/main/java/com/example/crane/buildsrc/Dependencies.kt
+++ Crane/buildSrc/src/main/java/com/example/crane/buildsrc/Dependencies.kt
@@ -100,7 +100,7 @@ object Libs {
     }
 
     object Hilt {
-        private const val version = "2.39"
+        private const val version = "2.42"
 
         const val gradlePlugin = "com.google.dagger:hilt-android-gradle-plugin:$version"
         const val android = "com.google.dagger:hilt-android:$version"
