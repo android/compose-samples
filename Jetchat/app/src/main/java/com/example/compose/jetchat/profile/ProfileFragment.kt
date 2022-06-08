e1dccf5d909e78ef4a360b576916cac290ecc2bc
Simona <35065668+simona-anomis@users.noreply.github.com>
2022-06-08 13:56:36 +0100

    [Jetchat] Add Nested scrolling interop API (#837)
    * Add Nested scrolling interop API to Jetchat
    
    * Disable the CollapsingToolbarLayout color change on scroll

--- Jetchat/app/src/main/java/com/example/compose/jetchat/profile/ProfileFragment.kt
+++ Jetchat/app/src/main/java/com/example/compose/jetchat/profile/ProfileFragment.kt
@@ -21,13 +21,31 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.outlined.MoreVert
+import androidx.compose.material3.Icon
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.livedata.observeAsState
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.ExperimentalComposeUiApi
+import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.ComposeView
+import androidx.compose.ui.platform.rememberNestedScrollInteropConnection
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.unit.dp
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.activityViewModels
 import androidx.fragment.app.viewModels
+import com.example.compose.jetchat.FunctionalityNotAvailablePopup
 import com.example.compose.jetchat.MainViewModel
+import com.example.compose.jetchat.R
+import com.example.compose.jetchat.components.JetchatAppBar
 import com.example.compose.jetchat.theme.JetchatTheme
 
 class ProfileFragment : Fragment() {
@@ -42,31 +60,61 @@ class ProfileFragment : Fragment() {
         viewModel.setUserId(userId)
     }
 
+    @OptIn(ExperimentalComposeUiApi::class)
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View = ComposeView(inflater.context).apply {
-        layoutParams = ViewGroup.LayoutParams(
-            ViewGroup.LayoutParams.MATCH_PARENT,
-            ViewGroup.LayoutParams.MATCH_PARENT
-        )
+    ): View {
+        val rootView: View = inflater.inflate(R.layout.fragment_profile, container, false)
 
-        setContent {
-            val userData by viewModel.userData.observeAsState()
+        rootView.findViewById<ComposeView>(R.id.toolbar_compose_view).apply {
+            setContent {
+                var functionalityNotAvailablePopupShown by remember { mutableStateOf(false) }
+                if (functionalityNotAvailablePopupShown) {
+                    FunctionalityNotAvailablePopup { functionalityNotAvailablePopupShown = false }
+                }
 
-            JetchatTheme {
-                if (userData == null) {
-                    ProfileError()
-                } else {
-                    ProfileScreen(
-                        userData = userData!!,
-                        onNavIconPressed = {
-                            activityViewModel.openDrawer()
+                JetchatTheme {
+                    JetchatAppBar(
+                        onNavIconPressed = { activityViewModel.openDrawer() },
+                        title = { },
+                        actions = {
+                            // More icon
+                            Icon(
+                                imageVector = Icons.Outlined.MoreVert,
+                                tint = MaterialTheme.colorScheme.onSurfaceVariant,
+                                modifier = Modifier
+                                    .clickable(onClick = {
+                                        functionalityNotAvailablePopupShown = true
+                                    })
+                                    .padding(horizontal = 12.dp, vertical = 16.dp)
+                                    .height(24.dp),
+                                contentDescription = stringResource(id = R.string.more_options)
+                            )
                         }
                     )
                 }
             }
         }
+
+        rootView.findViewById<ComposeView>(R.id.profile_compose_view).apply {
+            setContent {
+                val userData by viewModel.userData.observeAsState()
+                val nestedScrollInteropConnection = rememberNestedScrollInteropConnection()
+
+                JetchatTheme {
+                    if (userData == null) {
+                        ProfileError()
+                    } else {
+                        ProfileScreen(
+                            userData = userData!!,
+                            nestedScrollInteropConnection = nestedScrollInteropConnection
+                        )
+                    }
+                }
+            }
+        }
+        return rootView
     }
 }
