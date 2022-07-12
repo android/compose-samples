a7d3733f178c7ba876e630774c7f69b4528846a0
Odin Asbjørnsen <54936943+oas004@users.noreply.github.com>
2022-07-12 02:34:59 +0200

    Use derived state of for scrolled state to avoid recomposition on scrolling (#881)

--- JetNews/app/src/main/java/com/example/jetnews/utils/LazyListUtils.kt
+++ JetNews/app/src/main/java/com/example/jetnews/utils/LazyListUtils.kt
@@ -17,6 +17,7 @@
 package com.example.jetnews.utils
 
 import androidx.compose.foundation.lazy.LazyListState
+import androidx.compose.runtime.derivedStateOf
 
 val LazyListState.isScrolled: Boolean
-    get() = firstVisibleItemIndex > 0 || firstVisibleItemScrollOffset > 0
+    get() = derivedStateOf { firstVisibleItemIndex > 0 || firstVisibleItemScrollOffset > 0 }.value
