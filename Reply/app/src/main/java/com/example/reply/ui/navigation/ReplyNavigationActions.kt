be77f46717470b107aa8a4dc798a3ec84bbd9bf7
Gurupreet Singh <gurusingh@google.com>
2022-07-14 15:11:26 +0200

    fix the inverse color for dark mode

--- Reply/app/src/main/java/com/example/reply/ui/navigation/ReplyNavigationActions.kt
+++ Reply/app/src/main/java/com/example/reply/ui/navigation/ReplyNavigationActions.kt
@@ -18,9 +18,9 @@ package com.example.reply.ui.navigation
 
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Article
-import androidx.compose.material.icons.filled.Chat
 import androidx.compose.material.icons.filled.Inbox
 import androidx.compose.material.icons.filled.People
+import androidx.compose.material.icons.outlined.ChatBubbleOutline
 import androidx.compose.ui.graphics.vector.ImageVector
 import androidx.navigation.NavGraph.Companion.findStartDestination
 import androidx.navigation.NavHostController
@@ -74,8 +74,8 @@ val TOP_LEVEL_DESTINATIONS = listOf(
     ),
     ReplyTopLevelDestination(
         route = ReplyRoute.DM,
-        selectedIcon = Icons.Default.Chat,
-        unselectedIcon = Icons.Default.Chat,
+        selectedIcon = Icons.Outlined.ChatBubbleOutline,
+        unselectedIcon = Icons.Outlined.ChatBubbleOutline,
         iconTextId = R.string.tab_inbox
     ),
     ReplyTopLevelDestination(
