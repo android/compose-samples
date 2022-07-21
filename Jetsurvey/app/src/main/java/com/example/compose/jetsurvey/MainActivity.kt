16d80fee9bf845a6999f15b5c1108266c097c74f
deepika tripathi <deepika.tripathi@tokopedia.com>
2022-07-21 10:39:44 +0530

    changes

--- Jetsurvey/app/src/main/java/com/example/compose/jetsurvey/MainActivity.kt
+++ Jetsurvey/app/src/main/java/com/example/compose/jetsurvey/MainActivity.kt
@@ -23,11 +23,13 @@ import androidx.navigation.ui.setupWithNavController
 import com.google.android.material.navigation.NavigationView
 
 class MainActivity : AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         val navView: NavigationView = findViewById(R.id.nav_view)
         val navController = findNavController(R.id.nav_host_fragment)
         navView.setupWithNavController(navController)
+
     }
 }
