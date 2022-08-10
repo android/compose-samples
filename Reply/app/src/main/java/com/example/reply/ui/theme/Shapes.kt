660fb92a4cca0eefe18d7913040a7c7314b59cae
Gurupreet Singh <gurusingh@google.com>
2022-08-10 14:24:05 +0200

    Added a fix to not show back button on permanent drawer, added Material shapes

---
+++ Reply/app/src/main/java/com/example/reply/ui/theme/Shapes.kt
@@ -0,0 +1,13 @@
+package com.example.reply.ui.theme
+
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.Shapes
+import androidx.compose.ui.unit.dp
+
+val shapes = Shapes(
+    extraSmall = RoundedCornerShape(4.dp),
+    small = RoundedCornerShape(8.dp),
+    medium = RoundedCornerShape(16.dp),
+    large = RoundedCornerShape(24.dp),
+    extraLarge = RoundedCornerShape(32.dp)
+)
