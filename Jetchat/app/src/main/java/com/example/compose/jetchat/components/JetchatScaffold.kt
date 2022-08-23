808d386e11c55b5dfbddce6598ac7ad81ba30882
Rebecca Franks <riggaroo@google.com>
2022-08-23 14:49:35 +0100

    Downgrade to material 15

--- Jetchat/app/src/main/java/com/example/compose/jetchat/components/JetchatScaffold.kt
+++ Jetchat/app/src/main/java/com/example/compose/jetchat/components/JetchatScaffold.kt
@@ -20,11 +20,9 @@ import androidx.compose.material3.DrawerState
 import androidx.compose.material3.DrawerValue.Closed
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.ModalNavigationDrawer
-import androidx.compose.material3.ModalDrawerSheet
 import androidx.compose.material3.rememberDrawerState
 import androidx.compose.runtime.Composable
 import com.example.compose.jetchat.theme.JetchatTheme
-import com.example.compose.jetchat.components.JetchatDrawerContent
 
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
@@ -38,13 +36,9 @@ fun JetchatDrawer(
         ModalNavigationDrawer(
             drawerState = drawerState,
             drawerContent = {
-                ModalDrawerSheet(
-                    content = {
-                        JetchatDrawerContent(
-                            onProfileClicked = onProfileClicked,
-                            onChatClicked = onChatClicked
-                        )
-                    }
+                JetchatDrawerContent(
+                    onProfileClicked = onProfileClicked,
+                    onChatClicked = onChatClicked
                 )
             },
             content = content
