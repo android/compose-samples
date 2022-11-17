e6b75bc95b6bc3acf17eff1df51b3f59df76624c
Jolanda Verhoef <JolandaVerhoef@users.noreply.github.com>
2022-11-17 14:18:16 +0100

    [Jetsurvey] extract single answer for SingleChoiceIconQuestion and simplify code
    Change-Id: I006fcf3322555af4702d876130b01cc9143c58ab

--- Jetsurvey/app/src/main/java/com/example/compose/jetsurvey/survey/question/SingleChoiceIconQuestion.kt
+++ Jetsurvey/app/src/main/java/com/example/compose/jetsurvey/survey/question/SingleChoiceIconQuestion.kt
@@ -24,29 +24,41 @@ import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.selection.selectable
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.RadioButton
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.painter.Painter
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.tooling.preview.PreviewParameter
+import androidx.compose.ui.tooling.preview.PreviewParameterProvider
 import androidx.compose.ui.unit.dp
 import com.example.compose.jetsurvey.R
 import com.example.compose.jetsurvey.survey.Answer
 import com.example.compose.jetsurvey.survey.PossibleAnswer
 import com.example.compose.jetsurvey.theme.JetsurveyTheme
 
+/**
+ * Shows a list of possible answers with image and text and a radio button.
+ * @param possibleAnswer a list of all possible answers with their icon and text value
+ * @param answer the chosen answer (identified by text resource value), can be null if no answer is
+ * chosen
+ * @param onAnswerSelected callback that is called when a possible answer is clicked
+ * @param modifier Modifier to be applied to the [SingleChoiceIconQuestion]
+ */
 @Composable
 fun SingleChoiceIconQuestion(
     possibleAnswer: PossibleAnswer.SingleChoiceIcon,
@@ -54,78 +66,65 @@ fun SingleChoiceIconQuestion(
     onAnswerSelected: (Int) -> Unit,
     modifier: Modifier = Modifier
 ) {
-    val options = possibleAnswer.optionsStringIconRes.associateBy { stringResource(id = it.second) }
+    val options = possibleAnswer.optionsStringIconRes
 
-    val radioOptions = options.keys.toList()
-
-    val selected = if (answer != null) {
-        stringResource(id = answer.answer)
-    } else {
-        null
-    }
-
-    val (selectedOption, onOptionSelected) = remember(answer) { mutableStateOf(selected) }
-
-    Column(modifier = modifier) {
-        radioOptions.forEach { text ->
-            val onClickHandle = {
-                onOptionSelected(text)
-                options[text]?.let { onAnswerSelected(it.second) }
-                Unit
-            }
-            val optionSelected = text == selectedOption
-            val answerBorderColor = if (optionSelected) {
-                MaterialTheme.colorScheme.primary.copy(alpha = 0.5f)
-            } else {
-                MaterialTheme.colorScheme.onSurface.copy(alpha = 0.12f)
-            }
-            val answerBackgroundColor = if (optionSelected) {
-                MaterialTheme.colorScheme.primary.copy(alpha = 0.12f)
-            } else {
-                MaterialTheme.colorScheme.background
-            }
-            Surface(
-                shape = MaterialTheme.shapes.small,
-                border = BorderStroke(
-                    width = 1.dp,
-                    color = answerBorderColor
-                ),
+    Column(modifier) {
+        options.forEach { (imgRes, textRes) ->
+            SingleChoiceIconAnswer(
+                text = stringResource(textRes),
+                painter = painterResource(imgRes),
+                selected = textRes == answer?.answer,
+                onOptionSelected = { onAnswerSelected(textRes) },
                 modifier = Modifier.padding(vertical = 8.dp)
-            ) {
-                Row(
-                    modifier = Modifier
-                        .fillMaxWidth()
-                        .selectable(
-                            selected = optionSelected,
-                            onClick = onClickHandle
-                        )
-                        .background(answerBackgroundColor)
-                        .padding(vertical = 16.dp, horizontal = 16.dp),
-                    verticalAlignment = Alignment.CenterVertically,
-                    horizontalArrangement = Arrangement.SpaceBetween
-                ) {
-                    options[text]?.let {
-                        Image(
-                            painter = painterResource(
-                                id = it.first
-                            ),
-                            contentDescription = null,
-                            modifier = Modifier
-                                .width(56.dp)
-                                .height(56.dp)
-                                .clip(MaterialTheme.shapes.medium)
-                        )
-                    }
-                    Text(
-                        text = text
-                    )
+            )
+        }
+    }
+}
 
-                    RadioButton(
-                        selected = optionSelected,
-                        onClick = onClickHandle,
-                    )
-                }
-            }
+@Composable
+private fun SingleChoiceIconAnswer(
+    text: String,
+    painter: Painter,
+    selected: Boolean,
+    onOptionSelected: () -> Unit,
+    modifier: Modifier = Modifier
+) {
+    val answerBorderColor = if (selected) {
+        MaterialTheme.colorScheme.primary.copy(alpha = 0.5f)
+    } else {
+        MaterialTheme.colorScheme.onSurface.copy(alpha = 0.12f)
+    }
+    val answerBackgroundColor = if (selected) {
+        MaterialTheme.colorScheme.primary.copy(alpha = 0.12f)
+    } else {
+        MaterialTheme.colorScheme.background
+    }
+    Surface(
+        shape = MaterialTheme.shapes.small,
+        border = BorderStroke(width = 1.dp, color = answerBorderColor),
+        modifier = modifier
+    ) {
+        Row(
+            modifier = Modifier
+                .fillMaxWidth()
+                .selectable(
+                    selected = selected,
+                    onClick = onOptionSelected
+                )
+                .background(answerBackgroundColor)
+                .padding(vertical = 16.dp, horizontal = 16.dp),
+            verticalAlignment = Alignment.CenterVertically,
+            horizontalArrangement = Arrangement.SpaceBetween
+        ) {
+            Image(
+                painter = painter,
+                contentDescription = null,
+                modifier = Modifier
+                    .size(56.dp)
+                    .clip(MaterialTheme.shapes.medium)
+            )
+            Text(text)
+            RadioButton(selected = selected, onClick = null, modifier = Modifier.size(48.dp))
         }
     }
 }
@@ -133,7 +132,33 @@ fun SingleChoiceIconQuestion(
 @Preview(name = "Light", uiMode = Configuration.UI_MODE_NIGHT_NO)
 @Preview(name = "Dark", uiMode = Configuration.UI_MODE_NIGHT_YES)
 @Composable
+fun SingleChoiceIconAnswerPreview(
+    @PreviewParameter(SelectedProvider::class, limit = 2) selected: Boolean
+) {
+    JetsurveyTheme {
+        Surface {
+            SingleChoiceIconAnswer(
+                text = "Preview",
+                painter = painterResource(id = R.drawable.frag),
+                selected = selected,
+                onOptionSelected = { }
+            )
+        }
+    }
+}
+
+private class SelectedProvider : PreviewParameterProvider<Boolean> {
+    override val values = sequenceOf(false, true)
+}
+
+@Preview(name = "Light", uiMode = Configuration.UI_MODE_NIGHT_NO)
+@Preview(name = "Dark", uiMode = Configuration.UI_MODE_NIGHT_YES)
+@Composable
 fun SingleChoiceIconQuestionPreview() {
+    var selectedAnswer: Answer.SingleChoice? by remember {
+        mutableStateOf(Answer.SingleChoice(R.string.bugchaos))
+    }
+
     JetsurveyTheme {
         Surface {
             SingleChoiceIconQuestion(
@@ -145,8 +170,8 @@ fun SingleChoiceIconQuestionPreview() {
                         Pair(R.drawable.frag, R.string.frag)
                     )
                 ),
-                answer = null,
-                onAnswerSelected = {}
+                answer = selectedAnswer,
+                onAnswerSelected = { textRes -> selectedAnswer = Answer.SingleChoice(textRes) }
             )
         }
     }
