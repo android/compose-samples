7638260b675c7492a2134ac4603be78679ef23d3
Jolanda Verhoef <JolandaVerhoef@users.noreply.github.com>
2023-01-03 11:06:40 +0000

    Fix spotless issue
    Change-Id: I468af481f7893f6c950ace65ff231947b4b77ad7

--- Jetsnack/app/src/main/java/com/example/jetsnack/ui/home/Home.kt
+++ Jetsnack/app/src/main/java/com/example/jetsnack/ui/home/Home.kt
@@ -46,7 +46,6 @@ import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.remember
-import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
