cb20dc272f117bb55fcc7147ccf6304ffaaab1fe
Chris Arriola <carriola@google.com>
2023-03-22 13:20:13 -0700

    Use getLifecycle()

--- Owl/app/src/main/java/com/example/owl/ui/NavGraph.kt
+++ Owl/app/src/main/java/com/example/owl/ui/NavGraph.kt
@@ -148,4 +148,4 @@ class MainActions(navController: NavHostController) {
  * This is used to de-duplicate navigation events.
  */
 private fun NavBackStackEntry.lifecycleIsResumed() =
-    this.lifecycle.currentState == Lifecycle.State.RESUMED
+    this.getLifecycle().currentState == Lifecycle.State.RESUMED
