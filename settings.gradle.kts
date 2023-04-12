e0e863fe598f59d1d0a1ed3af42016ce478d2294
mlykotom <mlykotom@google.com>
2023-04-12 22:22:02 +0200

    Specify composite builds for root project

---
+++ settings.gradle.kts
@@ -0,0 +1,13 @@
+
+
+rootProject.name = "Jetpack Compose Samples"
+
+includeBuild("Crane")
+includeBuild("Jetcaster")
+includeBuild("Jetchat")
+includeBuild("JetLagged")
+includeBuild("JetNews")
+includeBuild("Jetsnack")
+includeBuild("Jetsurvey")
+includeBuild("Owl")
+includeBuild("Reply")
