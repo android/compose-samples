039cdbf84cd9e7ef5f71f752f222205fafa557ae
mlykotom <mlykotom@google.com>
2023-04-13 12:43:30 +0200

    Remove forgotten @Suppress("DSL_SCOPE_VIOLATION")

--- Jetcaster/build.gradle.kts
+++ Jetcaster/build.gradle.kts
@@ -14,7 +14,6 @@
  * limitations under the License.
  */
 
-@Suppress("DSL_SCOPE_VIOLATION") // TODO: Remove when updating to Gradle 8.1 (https://github.com/gradle/gradle/issues/22797)
 plugins {
     alias(libs.plugins.gradle.versions)
     alias(libs.plugins.version.catalog.update)
