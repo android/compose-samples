80e6ae8c094e0e75343a0b9d1d8c5268651545b1
mlykotom <mlykotom@google.com>
2023-04-13 11:19:36 +0200

    Fix AGP 8 breaking changes

--- Crane/app/build.gradle.kts
+++ Crane/app/build.gradle.kts
@@ -86,7 +86,7 @@ android {
 
     buildFeatures {
         compose = true
-
+        buildConfig = true
     }
 
     composeOptions {
--- Jetsnack/app/build.gradle.kts
+++ Jetsnack/app/build.gradle.kts
@@ -67,18 +67,12 @@ android {
     }
 
     compileOptions {
-        sourceCompatibility = JavaVersion.VERSION_1_8
-        targetCompatibility = JavaVersion.VERSION_1_8
+        sourceCompatibility = JavaVersion.VERSION_17
+        targetCompatibility = JavaVersion.VERSION_17
     }
 
     buildFeatures {
         compose = true
-        // Disable unused AGP features
-        buildConfig = false
-        aidl = false
-        renderScript = false
-        resValues = false
-        shaders = false
     }
 
     composeOptions {
