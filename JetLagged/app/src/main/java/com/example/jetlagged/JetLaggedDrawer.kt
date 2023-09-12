59ad546e86c886a26497dbeb0319daa0dd86783a
Rebecca Franks <riggaroo@google.com>
2023-09-11 20:46:39 -0400

    Cleanup

--- JetLagged/app/src/main/java/com/example/jetlagged/JetLaggedDrawer.kt
+++ JetLagged/app/src/main/java/com/example/jetlagged/JetLaggedDrawer.kt
@@ -16,7 +16,6 @@
 
 package com.example.jetlagged
 
-import android.util.Log
 import androidx.compose.animation.core.Animatable
 import androidx.compose.animation.core.calculateTargetValue
 import androidx.compose.animation.rememberSplineBasedDecay
@@ -36,7 +35,6 @@ import androidx.compose.material.icons.filled.Leaderboard
 import androidx.compose.material.icons.filled.Settings
 import androidx.compose.material3.Icon
 import androidx.compose.material3.NavigationDrawerItem
-import androidx.compose.material3.NavigationDrawerItemColors
 import androidx.compose.material3.NavigationDrawerItemDefaults
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
@@ -47,18 +45,13 @@ import androidx.compose.runtime.remember
 import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.geometry.Offset
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.graphicsLayer
 import androidx.compose.ui.graphics.vector.ImageVector
-import androidx.compose.ui.input.pointer.pointerInput
-import androidx.compose.ui.input.pointer.util.VelocityTracker
 import androidx.compose.ui.platform.LocalDensity
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.util.lerp
-import kotlinx.coroutines.cancel
 import kotlinx.coroutines.launch
-import kotlin.math.absoluteValue
 
 @Composable
 fun HomeScreenDrawer() {
