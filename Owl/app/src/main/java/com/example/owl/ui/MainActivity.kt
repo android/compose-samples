0bd1d7e4f9fb631571aebc3dc540ecfeedfaee06
Jolanda Verhoef <JolandaVerhoef@users.noreply.github.com>
2023-11-21 11:16:21 +0000

    [Owl] Add edge-to-edge support for Owl

--- Owl/app/src/main/java/com/example/owl/ui/MainActivity.kt
+++ Owl/app/src/main/java/com/example/owl/ui/MainActivity.kt
@@ -18,16 +18,21 @@ package com.example.owl.ui
 
 import android.os.Bundle
 import androidx.activity.ComponentActivity
+import androidx.activity.SystemBarStyle
 import androidx.activity.compose.setContent
-import androidx.core.view.WindowCompat
+import androidx.activity.enableEdgeToEdge
+import androidx.core.content.ContextCompat
+import com.example.owl.R
 
 class MainActivity : ComponentActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
+        enableEdgeToEdge(
+            statusBarStyle = SystemBarStyle.dark(
+                ContextCompat.getColor(this, R.color.immersive_sys_ui)
+            )
+        )
         super.onCreate(savedInstanceState)
-
-        // This app draws behind the system bars, so we want to handle fitting system windows
-        WindowCompat.setDecorFitsSystemWindows(window, false)
 
         setContent {
             OwlApp { finish() }
