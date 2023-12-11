266b0f54cd36d4c6bc7740aa5844f0bb3050249b
Gurupreet Singh <gurusingh@google.com>
2023-12-11 16:14:28 +0100

    cleanup

--- Reply/app/src/main/java/com/example/reply/ui/ReplyApp.kt
+++ Reply/app/src/main/java/com/example/reply/ui/ReplyApp.kt
@@ -24,7 +24,6 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.wrapContentWidth
 import androidx.compose.material3.Button
 import androidx.compose.material3.DrawerValue
 import androidx.compose.material3.ExperimentalMaterial3Api
