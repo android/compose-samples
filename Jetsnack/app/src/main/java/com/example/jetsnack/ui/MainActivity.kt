e94c57189ea3e6ad8e83e6d0f73618efeafb5f36
manabu-nakamura <ma.nakamura@gmail.com>
2024-02-15 10:39:51 +0900

    Jetsnack is laid out edge-to-edge in 2 & 3-button navigation mode

--- Jetsnack/app/src/main/java/com/example/jetsnack/ui/MainActivity.kt
+++ Jetsnack/app/src/main/java/com/example/jetsnack/ui/MainActivity.kt
@@ -20,11 +20,13 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.core.view.ViewCompat
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         enableEdgeToEdge()
         super.onCreate(savedInstanceState)
+        ViewCompat.setOnApplyWindowInsetsListener(window.decorView) { _, insets -> insets }
         setContent { JetsnackApp() }
     }
 }
