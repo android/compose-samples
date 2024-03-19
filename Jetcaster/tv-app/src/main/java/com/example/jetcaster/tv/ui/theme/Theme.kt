38b22d6c28032d0db874a65bc7994b4212e332ba
Chiko Shimizu <chikoski@google.com>
2024-03-19 15:31:19 +0900

    Fix format error in the tv-app module

--- Jetcaster/tv-app/src/main/java/com/example/jetcaster/tv/ui/theme/Theme.kt
+++ Jetcaster/tv-app/src/main/java/com/example/jetcaster/tv/ui/theme/Theme.kt
@@ -5,7 +5,7 @@
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
  *
- *      http://www.apache.org/licenses/LICENSE-2.0
+ *     https://www.apache.org/licenses/LICENSE-2.0
  *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
@@ -20,7 +20,6 @@ import androidx.compose.foundation.isSystemInDarkTheme
 import androidx.compose.runtime.Composable
 import androidx.tv.material3.ExperimentalTvMaterial3Api
 import androidx.tv.material3.MaterialTheme
-
 
 @OptIn(ExperimentalTvMaterial3Api::class)
 @Composable
