55bac65deb9de907665a63ee60e3b2d758ab1e25
Chiko Shimizu <chikoski@google.com>
2024-03-19 15:25:47 +0900

    Add license information

--- Jetcaster/tv-app/src/main/java/com/example/jetcaster/tv/MainActivity.kt
+++ Jetcaster/tv-app/src/main/java/com/example/jetcaster/tv/MainActivity.kt
@@ -1,16 +1,32 @@
+/*
+ * Copyright 2024 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package com.example.jetcaster.tv
 
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.tv.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.RectangleShape
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.tv.material3.ExperimentalTvMaterial3Api
 import androidx.tv.material3.Surface
+import androidx.tv.material3.Text
 import com.example.jetcaster.tv.ui.theme.JetcasterTheme
 
 class MainActivity : ComponentActivity() {
