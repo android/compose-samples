8899463fe5b8f9d734aa39c3ba9e21a2995dc13e
Chiko Shimizu <chikoski@google.com>
2024-04-05 14:29:39 +0900

    Update parameter retrieval code to use flow

--- Jetcaster/tv-app/src/main/java/com/example/jetcaster/tv/ui/episode/EpisodeScreenViewModel.kt
+++ Jetcaster/tv-app/src/main/java/com/example/jetcaster/tv/ui/episode/EpisodeScreenViewModel.kt
@@ -25,12 +25,14 @@ import com.example.jetcaster.core.data.repository.EpisodeStore
 import com.example.jetcaster.core.data.repository.PodcastsRepository
 import com.example.jetcaster.tv.ui.Screen
 import dagger.hilt.android.lifecycle.HiltViewModel
-import javax.inject.Inject
+import kotlinx.coroutines.ExperimentalCoroutinesApi
 import kotlinx.coroutines.flow.SharingStarted
+import kotlinx.coroutines.flow.flatMapLatest
 import kotlinx.coroutines.flow.flowOf
 import kotlinx.coroutines.flow.map
 import kotlinx.coroutines.flow.stateIn
 import kotlinx.coroutines.launch
+import javax.inject.Inject
 
 @HiltViewModel
 class EpisodeScreenViewModel @Inject constructor(
@@ -39,12 +41,15 @@ class EpisodeScreenViewModel @Inject constructor(
     episodeStore: EpisodeStore,
 ) : ViewModel() {
 
-    private val episodeUri = handle.get<String>(Screen.Episode.PARAMETER_NAME)
+    private val episodeUriFlow = handle.getStateFlow<String?>(Screen.Episode.PARAMETER_NAME, null)
 
-    private val episodeToPodcastFlow = if (episodeUri != null) {
-        episodeStore.episodeAndPodcastWithUri(episodeUri)
-    } else {
-        flowOf(null)
+    @OptIn(ExperimentalCoroutinesApi::class)
+    private val episodeToPodcastFlow = episodeUriFlow.flatMapLatest {
+        if (it != null) {
+            episodeStore.episodeAndPodcastWithUri(it)
+        } else {
+            flowOf(null)
+        }
     }.stateIn(
         viewModelScope,
         SharingStarted.WhileSubscribed(5_000),
