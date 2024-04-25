2877563336471d10c43174e29b55c6cfad3f8571
Chris Arriola <carriola@google.com>
2024-04-25 16:34:35 -0700

    [Jetcaster] Podcast details - adjusting spacing and show episode summary.

--- Jetcaster/app/src/main/java/com/example/jetcaster/ui/podcast/PodcastDetailsScreen.kt
+++ Jetcaster/app/src/main/java/com/example/jetcaster/ui/podcast/PodcastDetailsScreen.kt
@@ -181,7 +181,8 @@ fun PodcastDetailsContent(
                 onClick = navigateToPlayer,
                 onQueueEpisode = onQueueEpisode,
                 modifier = Modifier.fillMaxWidth(),
-                showPodcastImage = false
+                showPodcastImage = false,
+                showSummary = true
             )
         }
     }
