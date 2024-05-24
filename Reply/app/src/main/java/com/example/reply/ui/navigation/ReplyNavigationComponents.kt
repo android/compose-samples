28f097f3664adc25221eb5147095b3f7a334a48f
Jonathan Koren <jdkoren@google.com>
2024-05-24 14:56:09 -0700

    [Reply] add back the modal nav drawer

--- Reply/app/src/main/java/com/example/reply/ui/navigation/ReplyNavigationComponents.kt
+++ Reply/app/src/main/java/com/example/reply/ui/navigation/ReplyNavigationComponents.kt
@@ -33,6 +33,7 @@ import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.automirrored.filled.MenuOpen
 import androidx.compose.material.icons.filled.Edit
 import androidx.compose.material.icons.filled.Menu
+import androidx.compose.material3.DrawerValue
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.ExtendedFloatingActionButton
 import androidx.compose.material3.FloatingActionButton
@@ -40,6 +41,7 @@ import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.ModalDrawerSheet
+import androidx.compose.material3.ModalNavigationDrawer
 import androidx.compose.material3.NavigationBar
 import androidx.compose.material3.NavigationBarItem
 import androidx.compose.material3.NavigationDrawerItem
@@ -52,7 +54,9 @@ import androidx.compose.material3.adaptive.currentWindowAdaptiveInfo
 import androidx.compose.material3.adaptive.currentWindowSize
 import androidx.compose.material3.adaptive.navigationsuite.NavigationSuiteScaffoldLayout
 import androidx.compose.material3.adaptive.navigationsuite.NavigationSuiteType
+import androidx.compose.material3.rememberDrawerState
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
@@ -71,6 +75,7 @@ import androidx.window.core.layout.WindowSizeClass
 import androidx.window.core.layout.WindowWidthSizeClass
 import com.example.reply.R
 import com.example.reply.ui.utils.ReplyNavigationContentPosition
+import kotlinx.coroutines.launch
 
 private fun WindowSizeClass.isCompact() =
     windowWidthSizeClass == WindowWidthSizeClass.COMPACT ||
@@ -99,28 +104,52 @@ fun ReplyNavigationWrapper(
         else -> NavigationSuiteType.NavigationRail
     }
 
-    NavigationSuiteScaffoldLayout(
-        layoutType = navLayoutType,
-        navigationSuite = {
-            when (navLayoutType) {
-                NavigationSuiteType.NavigationBar -> ReplyBottomNavigationBar(
-                    selectedDestination = selectedDestination,
-                    navigateToTopLevelDestination = navigateToTopLevelDestination
-                )
-                NavigationSuiteType.NavigationRail -> ReplyNavigationRail(
-                    selectedDestination = selectedDestination,
-                    navigationContentPosition = ReplyNavigationContentPosition.CENTER,
-                    navigateToTopLevelDestination = navigateToTopLevelDestination
-                )
-                NavigationSuiteType.NavigationDrawer -> PermanentNavigationDrawerContent(
-                    selectedDestination = selectedDestination,
-                    navigationContentPosition = ReplyNavigationContentPosition.CENTER,
-                    navigateToTopLevelDestination = navigateToTopLevelDestination
-                )
-            }
-        }
+    val drawerState = rememberDrawerState(initialValue = DrawerValue.Closed)
+    val coroutineScope = rememberCoroutineScope()
+
+    ModalNavigationDrawer(
+        drawerState = drawerState,
+        drawerContent = {
+            ModalNavigationDrawerContent(
+                selectedDestination = selectedDestination,
+                navigationContentPosition = ReplyNavigationContentPosition.CENTER,
+                navigateToTopLevelDestination = navigateToTopLevelDestination,
+                onDrawerClicked = {
+                    coroutineScope.launch {
+                        drawerState.close()
+                    }
+                }
+            )
+        },
     ) {
-        ReplyNavSuiteScope(navLayoutType).content()
+        NavigationSuiteScaffoldLayout(
+            layoutType = navLayoutType,
+            navigationSuite = {
+                when (navLayoutType) {
+                    NavigationSuiteType.NavigationBar -> ReplyBottomNavigationBar(
+                        selectedDestination = selectedDestination,
+                        navigateToTopLevelDestination = navigateToTopLevelDestination
+                    )
+                    NavigationSuiteType.NavigationRail -> ReplyNavigationRail(
+                        selectedDestination = selectedDestination,
+                        navigationContentPosition = ReplyNavigationContentPosition.CENTER,
+                        navigateToTopLevelDestination = navigateToTopLevelDestination,
+                        onDrawerClicked = {
+                            coroutineScope.launch {
+                                drawerState.open()
+                            }
+                        }
+                    )
+                    NavigationSuiteType.NavigationDrawer -> PermanentNavigationDrawerContent(
+                        selectedDestination = selectedDestination,
+                        navigationContentPosition = ReplyNavigationContentPosition.CENTER,
+                        navigateToTopLevelDestination = navigateToTopLevelDestination
+                    )
+                }
+            }
+        ) {
+            ReplyNavSuiteScope(navLayoutType).content()
+        }
     }
 }
 
