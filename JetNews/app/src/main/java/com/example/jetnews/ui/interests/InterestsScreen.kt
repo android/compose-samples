1936fffad619e434c230a6ee9ecd06c0a5e711f3
Jamal Eason <jamal.eason@gmail.com>
2024-07-08 12:10:17 -0700

    Revert "Merge branch 'jetnews-article-update' of https://github.com/pedrosax/compose-samples into pr/1426"
    This reverts commit a36cc181463d691a6598cab89b6f3833c30b6a80, reversing
    changes made to 53b2c56a2c9c43235bb6c5827c4149e2e0c0fb86.

--- JetNews/app/src/main/java/com/example/jetnews/ui/interests/InterestsScreen.kt
+++ JetNews/app/src/main/java/com/example/jetnews/ui/interests/InterestsScreen.kt
@@ -38,6 +38,7 @@ import androidx.compose.foundation.verticalScroll
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Search
 import androidx.compose.material3.CenterAlignedTopAppBar
+import androidx.compose.material3.Divider
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.HorizontalDivider
 import androidx.compose.material3.Icon
