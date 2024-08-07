91878de1ad6d91df0ba3d78b2f98786f351bb714
Rebecca Franks <riggaroo@google.com>
2024-08-07 11:40:29 +0100

    Update deps for all samples

--- scripts/duplicate_version_config.sh
+++ scripts/duplicate_version_config.sh
@@ -25,32 +25,23 @@
 
 set -xe
 
-cp scripts/libs.versions.toml Crane/gradle/libs.versions.toml
 cp scripts/libs.versions.toml Jetcaster/gradle/libs.versions.toml
 cp scripts/libs.versions.toml Jetchat/gradle/libs.versions.toml
 cp scripts/libs.versions.toml JetLagged/gradle/libs.versions.toml
 cp scripts/libs.versions.toml JetNews/gradle/libs.versions.toml
 cp scripts/libs.versions.toml Jetsnack/gradle/libs.versions.toml
-cp scripts/libs.versions.toml Jetsurvey/gradle/libs.versions.toml
-cp scripts/libs.versions.toml Owl/gradle/libs.versions.toml
 cp scripts/libs.versions.toml Reply/gradle/libs.versions.toml
 
-cp scripts/toml-updater-config.gradle Crane/buildscripts/toml-updater-config.gradle
 cp scripts/toml-updater-config.gradle Jetcaster/buildscripts/toml-updater-config.gradle
 cp scripts/toml-updater-config.gradle Jetchat/buildscripts/toml-updater-config.gradle
 cp scripts/toml-updater-config.gradle JetLagged/buildscripts/toml-updater-config.gradle
 cp scripts/toml-updater-config.gradle JetNews/buildscripts/toml-updater-config.gradle
 cp scripts/toml-updater-config.gradle Jetsnack/buildscripts/toml-updater-config.gradle
-cp scripts/toml-updater-config.gradle Jetsurvey/buildscripts/toml-updater-config.gradle
-cp scripts/toml-updater-config.gradle Owl/buildscripts/toml-updater-config.gradle
 cp scripts/toml-updater-config.gradle Reply/buildscripts/toml-updater-config.gradle
 
-cp scripts/init.gradle.kts Crane/buildscripts/init.gradle.kts
 cp scripts/init.gradle.kts Jetcaster/buildscripts/init.gradle.kts
 cp scripts/init.gradle.kts Jetchat/buildscripts/init.gradle.kts
 cp scripts/init.gradle.kts JetLagged/buildscripts/init.gradle.kts
 cp scripts/init.gradle.kts JetNews/buildscripts/init.gradle.kts
 cp scripts/init.gradle.kts Jetsnack/buildscripts/init.gradle.kts
-cp scripts/init.gradle.kts Jetsurvey/buildscripts/init.gradle.kts
-cp scripts/init.gradle.kts Owl/buildscripts/init.gradle.kts
 cp scripts/init.gradle.kts Reply/buildscripts/init.gradle.kts
