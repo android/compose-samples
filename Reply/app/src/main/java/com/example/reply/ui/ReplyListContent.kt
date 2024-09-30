7a399c353299caf0ec9ed40690eed5a5e1db4398
jamilxt <jamilxt@gmail.com>
2024-09-30 17:37:29 +0600

    use mutableIntStateOf instead of mutableStateOf

--- Reply/app/src/main/java/com/example/reply/ui/ReplyListContent.kt
+++ Reply/app/src/main/java/com/example/reply/ui/ReplyListContent.kt
@@ -241,7 +241,7 @@ fun ReplyEmailDetail(
 @Composable
 private fun LazyListState.isScrollingUp(): Boolean {
     var previousIndex by remember(this) { mutableIntStateOf(firstVisibleItemIndex) }
-    var previousScrollOffset by remember(this) { mutableStateOf(firstVisibleItemScrollOffset) }
+    var previousScrollOffset by remember(this) { mutableIntStateOf(firstVisibleItemScrollOffset) }
     return remember(this) {
         derivedStateOf {
             if (previousIndex != firstVisibleItemIndex) {
