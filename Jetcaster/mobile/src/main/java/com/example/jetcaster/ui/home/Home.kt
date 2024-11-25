b956abd0e9d20d1162220c9e6c031e7bd2d7d534
yongsuk44 <rlarladyd@gmail.com>
2024-11-25 21:04:54 +0900

    Spotless apply

--- Jetcaster/mobile/src/main/java/com/example/jetcaster/ui/home/Home.kt
+++ Jetcaster/mobile/src/main/java/com/example/jetcaster/ui/home/Home.kt
@@ -22,11 +22,9 @@ import androidx.activity.compose.BackHandler
 import androidx.compose.foundation.ExperimentalFoundationApi
 import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
-import androidx.compose.foundation.gestures.snapping.SnapPosition
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.BoxScope
-import androidx.compose.foundation.layout.BoxWithConstraints
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.Row
