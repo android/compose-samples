1d955528414d3be1e5c0084481f3b3c4c18ce613
Tomáš Mlynarič <mlykotom@google.com>
2024-12-17 11:44:08 +0100

    Remove unused home lazylist items
    # Conflicts:
    #	Jetcaster/mobile/src/main/java/com/example/jetcaster/ui/home/discover/Discover.kt

--- Jetcaster/mobile/src/main/java/com/example/jetcaster/ui/home/library/Library.kt
+++ Jetcaster/mobile/src/main/java/com/example/jetcaster/ui/home/library/Library.kt
@@ -18,10 +18,8 @@ package com.example.jetcaster.ui.home.library
 
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.lazy.LazyListScope
 import androidx.compose.foundation.lazy.grid.LazyGridScope
 import androidx.compose.foundation.lazy.grid.items
-import androidx.compose.foundation.lazy.items
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.ui.Modifier
@@ -34,36 +32,6 @@ import com.example.jetcaster.core.player.model.PlayerEpisode
 import com.example.jetcaster.designsystem.theme.Keyline1
 import com.example.jetcaster.ui.shared.EpisodeListItem
 import com.example.jetcaster.util.fullWidthItem
-
-fun LazyListScope.libraryItems(
-    library: LibraryInfo,
-    navigateToPlayer: (EpisodeInfo) -> Unit,
-    onQueueEpisode: (PlayerEpisode) -> Unit
-) {
-    item {
-        Text(
-            text = stringResource(id = R.string.latest_episodes),
-            modifier = Modifier.padding(
-                start = Keyline1,
-                top = 16.dp,
-            ),
-            style = MaterialTheme.typography.titleLarge,
-        )
-    }
-
-    items(
-        library,
-        key = { it.episode.uri }
-    ) { item ->
-        EpisodeListItem(
-            episode = item.episode,
-            podcast = item.podcast,
-            onClick = navigateToPlayer,
-            onQueueEpisode = onQueueEpisode,
-            modifier = Modifier.fillParentMaxWidth(),
-        )
-    }
-}
 
 fun LazyGridScope.libraryItems(
     library: LibraryInfo,
