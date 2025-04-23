52154e0f0ffe45cba219cadda2e667a2fa169672
Don Turner <donturner@google.com>
2025-04-24 00:25:48 +0100

    Update dependencies and fix associated issues

--- Jetcaster/mobile/src/main/java/com/example/jetcaster/ui/home/Home.kt
+++ Jetcaster/mobile/src/main/java/com/example/jetcaster/ui/home/Home.kt
@@ -264,8 +264,11 @@ private fun HomeScreenReady(
     val navigator = rememberSupportingPaneScaffoldNavigator<String>(
         scaffoldDirective = calculateScaffoldDirective(currentWindowAdaptiveInfo())
     )
+
+    val scope = rememberCoroutineScope()
+
     BackHandler(enabled = navigator.canNavigateBack()) {
-        navigator.navigateBack()
+        scope.launch { navigator.navigateBack() }
     }
 
     Surface {
@@ -284,14 +287,16 @@ private fun HomeScreenReady(
                     library = uiState.library,
                     onHomeAction = viewModel::onHomeAction,
                     navigateToPodcastDetails = {
-                        navigator.navigateTo(SupportingPaneScaffoldRole.Supporting, it.uri)
+                        scope.launch {
+                            navigator.navigateTo(SupportingPaneScaffoldRole.Supporting, it.uri)
+                        }
                     },
                     navigateToPlayer = navigateToPlayer,
                     modifier = Modifier.fillMaxSize()
                 )
             },
             supportingPane = {
-                val podcastUri = navigator.currentDestination?.content
+                val podcastUri = navigator.currentDestination?.contentKey
                 if (!podcastUri.isNullOrEmpty()) {
                     val podcastDetailsViewModel =
                         hiltViewModel<PodcastDetailsViewModel, PodcastDetailsViewModel.Factory>(
@@ -304,7 +309,9 @@ private fun HomeScreenReady(
                         navigateToPlayer = navigateToPlayer,
                         navigateBack = {
                             if (navigator.canNavigateBack()) {
-                                navigator.navigateBack()
+                                scope.launch {
+                                    navigator.navigateBack()
+                                }
                             }
                         },
                         showBackButton = navigator.isMainPaneHidden(),
