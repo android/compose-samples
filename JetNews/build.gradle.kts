78dcce9a6eec8686e826036ba6051f2c820aa94a
Tomáš Mlynarič <mlykotom@google.com>
2025-05-16 09:22:59 +0200

    Fix spotless configuration for all samples except Jetcaster
    Jetcaster format is fixed in a separate PR.
    I'm doing it this way to prevent million of conflicts, because Jetcaster has major redesign

--- JetNews/build.gradle.kts
+++ JetNews/build.gradle.kts
@@ -29,45 +29,18 @@ apply("${project.rootDir}/buildscripts/toml-updater-config.gradle")
 subprojects {
     apply(plugin = "com.diffplug.spotless")
     configure<com.diffplug.gradle.spotless.SpotlessExtension> {
-        ratchetFrom = "origin/main"
         kotlin {
             target("**/*.kt")
-            targetExclude("**/build/**/*.kt")
-            ktlint().editorConfigOverride(
-                mapOf(
-                    "ktlint_code_style" to "android_studio",
-                    "ij_kotlin_allow_trailing_comma" to true,
-                    "ktlint_function_naming_ignore_when_annotated_with" to "Composable",
-                    // These rules were introduced in ktlint 0.46.0 and should not be
-                    // enabled without further discussion. They are disabled for now.
-                    // See: https://github.com/pinterest/ktlint/releases/tag/0.46.0
-                    "disabled_rules" to
-                            "filename," +
-                            "annotation,annotation-spacing," +
-                            "argument-list-wrapping," +
-                            "double-colon-spacing," +
-                            "enum-entry-name-case," +
-                            "multiline-if-else," +
-                            "no-empty-first-line-in-method-block," +
-                            "package-name," +
-                            "trailing-comma," +
-                            "spacing-around-angle-brackets," +
-                            "spacing-between-declarations-with-annotations," +
-                            "spacing-between-declarations-with-comments," +
-                            "unary-op-spacing"
-                )
-            )
+            targetExclude("${layout.buildDirectory}/**/*.kt")
+            ktlint()
             licenseHeaderFile(rootProject.file("spotless/copyright.kt"))
-        }
-        format("kts") {
-            target("**/*.kts")
-            targetExclude("**/build/**/*.kts")
-            // Look for the first line that doesn't have a block comment (assumed to be the license)
-            licenseHeaderFile(rootProject.file("spotless/copyright.kt"), "(^(?![\\/ ]\\*).*$)")
         }
         kotlinGradle {
             target("*.gradle.kts")
+            targetExclude("${layout.buildDirectory}/**/*.kt")
             ktlint()
+            // Look for the first line that doesn't have a block comment (assumed to be the license)
+            licenseHeaderFile(rootProject.file("spotless/copyright.kt"), "(^(?![\\/ ]\\*).*$)")
         }
     }
 }
