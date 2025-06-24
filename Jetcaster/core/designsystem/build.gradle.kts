e5f5562264bb8c0626077abc112c4fdffd28912e
Ajesh R <ajeshr@google.com>
2025-06-24 15:45:24 +0530

    Fixes: formatting

--- Jetcaster/core/designsystem/build.gradle.kts
+++ Jetcaster/core/designsystem/build.gradle.kts
@@ -51,13 +51,14 @@ android {
         buildConfig = true
     }
 
-  compileOptions {
-    sourceCompatibility = JavaVersion.VERSION_17
-    targetCompatibility = JavaVersion.VERSION_17
-  }
-  kotlinOptions {
-    jvmTarget = "17"
-  }
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_17
+        targetCompatibility = JavaVersion.VERSION_17
+    }
+
+    kotlinOptions {
+        jvmTarget = "17"
+    }
 }
 
 dependencies {
