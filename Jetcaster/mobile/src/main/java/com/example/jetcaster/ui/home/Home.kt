2705729ede73920442e3ed9e719d10c1a534e55c
ashnohe <ashnohe@google.com>
2025-10-16 17:46:56 -0700

    fix PillToolbar padding

--- Jetcaster/mobile/src/main/java/com/example/jetcaster/ui/home/Home.kt
+++ Jetcaster/mobile/src/main/java/com/example/jetcaster/ui/home/Home.kt
@@ -33,6 +33,7 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.navigationBars
+import androidx.compose.foundation.layout.navigationBarsPadding
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.windowInsetsPadding
@@ -467,7 +468,7 @@ private fun HomeScreen(
 @Composable
 fun PillToolbar(selectedHomeCategory: HomeCategory, onHomeAction: (HomeAction) -> Unit, modifier: Modifier = Modifier) {
     HorizontalFloatingToolbar(
-        modifier = modifier,
+        modifier = modifier.navigationBarsPadding(),
         colors = FloatingToolbarColors(
             toolbarContainerColor = MaterialTheme.colorScheme.surfaceContainerHighest,
             toolbarContentColor = MaterialTheme.colorScheme.onSurfaceVariant,
