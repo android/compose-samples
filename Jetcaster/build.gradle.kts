14965bf8602e2f65c4a7c68e4e325bc0b65b6f88
Jaebaek Seo <jaebaek@google.com>
2026-02-02 19:49:58 +0000

    Migrate Jetcaster to AGP 9.0
    GiAS prepared this by relying on AKB tools.

--- Jetcaster/build.gradle.kts
+++ Jetcaster/build.gradle.kts
@@ -19,7 +19,6 @@ plugins {
     alias(libs.plugins.version.catalog.update)
     alias(libs.plugins.android.application) apply false
     alias(libs.plugins.android.library) apply false
-    alias(libs.plugins.kotlin.android) apply false
     alias(libs.plugins.kotlin.parcelize) apply false
     alias(libs.plugins.hilt) apply false
     alias(libs.plugins.ksp) apply false
--- Jetcaster/core/data/build.gradle.kts
+++ Jetcaster/core/data/build.gradle.kts
@@ -19,7 +19,6 @@ import org.jetbrains.kotlin.gradle.dsl.JvmTarget
 
 plugins {
     alias(libs.plugins.android.library)
-    alias(libs.plugins.kotlin.android)
     alias(libs.plugins.ksp)
     alias(libs.plugins.hilt)
 }
