f4eeb70ca51841708532c7b42ece6bcd50687cb5
Ben Sagmoe <bsagmoe@google.com>
2026-04-07 14:17:04 -0400

    Update Preview device parameters for DevicePreviews

--- Jetcaster/mobile/src/main/java/com/example/jetcaster/ui/tooling/DevicePreviews.kt
+++ Jetcaster/mobile/src/main/java/com/example/jetcaster/ui/tooling/DevicePreviews.kt
@@ -21,7 +21,7 @@ import androidx.compose.ui.tooling.preview.Preview
 
 @Preview(name = "small-phone", device = Devices.PIXEL_4A)
 @Preview(name = "phone", device = Devices.PHONE)
-@Preview(name = "landscape", device = "spec:shape=Normal,width=640,height=360,unit=dp,dpi=480")
+@Preview(name = "landscape", device = "spec:width=640dp,height=360dp,dpi=480")
 @Preview(name = "foldable", device = Devices.FOLDABLE)
 @Preview(name = "tablet", device = Devices.TABLET)
 annotation class DevicePreviews
